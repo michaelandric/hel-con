b01a31d08375b49a51c05455798e647f96721a30
Michael Andric <andric.michael@gmail.com>
2015-02-22 14:49:37 +0100

    get_ijk update

--- 3.maskdump.py
+++ 3.maskdump.py
@@ -3,7 +3,7 @@
 import time
 import os
 from shlex import split
-from subprocess import call
+from subprocess import call, Popen, PIPE
 
 
 def maskdump(inimage, outname, mask=None):
@@ -21,6 +21,20 @@ def maskdump(inimage, outname, mask=None):
     f.close()
     print 'DONE. '+time.ctime()
 
+def get_ijk(outname, mask=None):
+    print 'Getting IJK coordinates... '+time.ctime()
+    f = open('stdout_files/stderr_from_getijk.txt', 'w')
+    dmpout = open('%s' % outname, 'w')
+    if not mask==None:
+        print 'USING MASK: '+mask
+        cmdargs = split("3dmaskdump -mask %s %s | awk '{print $1, $2, $3}'" % (mask, mask))
+    else:
+        print 'NOT USING MASK.'
+        cmdargs = split("3dmaskdump %s | awk '{print $1, $2, $3}'" % mask)
+    call(cmdargs, stdout=dmpout, stderr=f)
+
+
+
 if __name__ == '__main__':
 
     subj_list = ['hel19']
@@ -33,4 +47,6 @@ if __name__ == '__main__':
         inimage = os.environ['hel']+'/%s/connectivity/cleanTScat_%s.allruns+orig' % (ss, ss)
         outname = os.environ['hel']+'/%s/connectivity/cleanTScat_%s.allruns_GMmask_dump' % (ss, ss)
         mask = os.environ['hel']+'/%s/volume.%s.anat/%s_GMmask_resampled+orig' % (ss, ss, ss)
-        maskdump(inimage, outname, mask)
+        #maskdump(inimage, outname, mask)
+        outname = os.environ['hel']+'/%s/connectivity/%s.ijk_GMmask_dump' % (ss, ss)
+        get_ijk(outname, mask)
