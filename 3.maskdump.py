0ce456aea09c77b558e52ba2b7f400b5c3675b78
Michael Andric <andric.michael@gmail.com>
2015-02-27 17:20:15 +0100

    clean up

--- 3.maskdump.py
+++ 3.maskdump.py
@@ -34,18 +34,19 @@ def get_ijk(outname, mask=None):
 
 if __name__ == '__main__':
 
-    subj_list = ['hel19']
+    subj_list = ['hel18']
     for ss in subj_list:
         os.chdir(os.environ['hel']+'/%s/connectivity/' % ss)
         print os.getcwd()
         stdfdir = os.environ['hel']+'/%s/connectivity/stdout_files' % (ss)
         if not os.path.exists(stdfdir):
             os.makedirs(stdfdir)
+
         inimage = os.environ['hel']+'/%s/connectivity/cleanTScat_%s.allruns+orig' % (ss, ss)
         outname = os.environ['hel']+'/%s/connectivity/cleanTScat_%s.allruns_GMmask_dump' % (ss, ss)
         mask = os.environ['hel']+'/%s/volume.%s.anat/%s_GMmask_resampled+orig' % (ss, ss, ss)
-        #maskdump(inimage, outname, mask)
-        os.chdir(os.environ['hel']+'/%s/volume.%s.anat/' % (ss,ss))
+        maskdump(inimage, outname, mask)
+        os.chdir(os.environ['hel']+'/%s/volume.%s.anat/' % (ss, ss))
         print os.getcwd()
         outname = '%s.ijk_GMmask_dump' % ss
         mask = '%s_GMmask_resampled+orig.BRIK.gz' % ss
