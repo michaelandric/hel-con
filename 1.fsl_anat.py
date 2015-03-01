5ed10e4265214ffffdfc7efa0f96ff4c518e2036
Michael Andric <andric.michael@gmail.com>
2015-03-01 13:45:13 +0100

    changed ss

--- 1.fsl_anat.py
+++ 1.fsl_anat.py
@@ -34,7 +34,7 @@ class FSLANAT:
 
 if __name__ == "__main__":
 
-    subj_list = ['hel8']
+    subj_list = ['hel8', 'hel10', 'hel11']
 
     for ss in subj_list:
         os.chdir(os.environ['hel']+'/%s' % ss)
