e4f7a1c4b2779d467b9ef82db1af2267081b12a1
Michael Andric <andric.michael@gmail.com>
2015-03-01 15:54:19 +0100

    ss

--- 2.gm_mask.py
+++ 2.gm_mask.py
@@ -51,7 +51,7 @@ class MaskMaker:
 
 if __name__ == "__main__":
 
-    subj_list = ['hel1', 'hel2', 'hel3']
+    subj_list = ['hel4', 'hel5', 'hel6']
     for ss in subj_list:
         os.chdir(os.environ['hel']+'/%s/volume.%s.anat' % (ss, ss))
 
