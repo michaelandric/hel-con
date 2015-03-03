89f66eb2553ac95e7dfbe073403b7421779814c5
Michael Andric <andric.michael@gmail.com>
2015-03-03 14:56:51 +0100

    methods for comparing partitions

---
+++ 9.compare_partitions.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""Compare partitions using Rand Index"""
+__author__ = 'andric'
+
+import sys
+import time
+import numpy as np
+
+class COMPARE:
+
+    def adjRand(self, p1, p2):
+        ars = adjusted_rand_score(p1, p2)
+        return ars
+
+    def normalized_MI(self, p1, p2):
+        nmi = normalized_mutual_info_score(p1, p2)
+        return nmi
+
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 5:
+    sys.stderr.write("You done screwed up! \n"
+                     "Usage: %s <SUBJECT ID> <CONDITION ID> <THRESH DENSITY> <METHOD>  \n" %
+                     (os.path.basename(sys.argv[0]),))
+
+    subjid = sys.argv[1]
+    condition = sys.argv[2]
+    thresh_density = sys.argv[3]
+    compare_method = sys.argv[4]
+    niter = 100   # because I have 100 iterations of the modularity solution, one tree for each
+
+    n_regions = 148
+    # Building array of inputs. These are the trees of highest modularity
+    tree_mat = np.array(np.zeros(niter*n_regions)).reshape(n_regions, niter)
+    for n in xrange(niter):
+        infile = 'tree_highest/%s_dens%s_tree.iter%d.highesttree' % (subjid, thresh_density, n)
+        tree_mat[:,n] = np.loadtxt(infile)[:,1]   # because these infile actually has one col for indices
+
+    # Prepping output array
+    n_combinations = ((niter**2)-niter)/2
+    compare_out = np.array(np.zeros(n_combinations))
+
+    cmp = COMPARE()
+
+    if compare_method == 'ARI':
+        from sklearn.metrics import adjusted_rand_score
+        for combo in combinations(np.arange(100), 2):
+            compare_out = cmp.adjRand(tree_mat[:,combo[0]], tree_mat[:,combo[1]])
+    elif compare_method == 'NMI':
+        from sklearn.metrics import normalized_mutual_info_score
+        for combo in combinations(np.arange(100), 2):
+            compare_out = cmp.normalized_MI(tree_mat[:,combo[0]], tree_mat[:,combo[1]])
+    else:
+        print 'Where is your method?? \nFlaming... '+time.ctime()
+        sys.exit(1)
+
+    return compare_out
