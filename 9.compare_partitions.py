7d20e72066ae240b03801e9516b4fcd8c6c40a75
Michael Andric <andric.michael@gmail.com>
2015-03-03 16:19:28 +0100

    tweaked to do 2 ss

--- 9.compare_partitions.py
+++ 9.compare_partitions.py
@@ -41,40 +41,44 @@ if __name__ == '__main__':
 
     if len(sys.argv) < 3:
         sys.stderr.write("You done screwed up! \n"
-                         "Usage: %s <SUBJECT ID> <METHOD> \n" %
+                         "Usage: %s <SUBJECT ID> <COMPARE METHOD> \n" %
                          (os.path.basename(sys.argv[0]),))
 
     subjid = sys.argv[1]
     compare_method = sys.argv[2]
     niter = 100   # because I have 100 iterations of the modularity solution, one tree for each
-    output_pref = '%s_%s.txt' % (subjid, compare_method)
+    n_combinations = ((niter**2)-niter)/2
+    compare_out = np.array(np.zeros(n_combinations))   # prep output array
+    output_pref = '%s_%s.txt' % (subjid, compare_method)   # output naming prefix
 
     n_regions = 148
     # Building array of inputs. These are the trees of highest modularity
-    tree_mat = np.array(np.zeros(niter*n_regions)).reshape(n_regions, niter)
+    tree_mat1 = np.array(np.zeros(niter*n_regions)).reshape(n_regions, niter)
+    tree_mat2 = np.array(np.zeros(niter*n_regions)).reshape(n_regions, niter)
     for n in xrange(niter):
-        infile = 'tree_highest/%s_dens0.1_tree.iter%d.highesttree' % (subjid, n)
-        tree_mat[:,n] = np.loadtxt(infile)[:,1]   # because these infile actually has one col for indices
+        infile1 = 'tree_highest/%s_dens0.1_tree.iter%d.highesttree' % ('CB', n)
+        tree_mat1[:,n] = np.loadtxt(infile1)[:,1]   # because these infile actually has one col for indices
+        infile2 = 'tree_highest/%s_dens0.1_tree.iter%d.highesttree' % ('SCB', n)
+        tree_mat2[:,n] = np.loadtxt(infile1)[:,1]   # because these infile actually has one col for indices
 
-    # Prepping output array
-    n_combinations = ((niter**2)-niter)/2
+    #tree_mat2 = tree_mat1   # If comparing for same subject over iterations
 
+
+    # Main section to run
     cmp = COMPARE()
 
     if compare_method == 'ARI':
-        compare_out = np.array(np.zeros(n_combinations))
         from sklearn.metrics import adjusted_rand_score
         i = 0
         for combo in combinations(np.arange(100), 2):
-            compare_out[i] = cmp.adjRand(tree_mat[:,combo[0]], tree_mat[:,combo[1]])
+            compare_out[i] = cmp.adjRand(tree_mat1[:,combo[0]], tree_mat2[:,combo[1]])
             i = i+1
         np.savetxt(output_pref, compare_out, fmt='%.4f')
     elif compare_method == 'NMI':
-        compare_out = np.array(np.zeros(n_combinations))
         from sklearn.metrics import normalized_mutual_info_score
         i = 0
         for combo in combinations(np.arange(100), 2):
-            compare_out[i] = cmp.normalized_MI(tree_mat[:,combo[0]], tree_mat[:,combo[1]])
+            compare_out[i] = cmp.normalized_MI(tree_mat1[:,combo[0]], tree_mat2[:,combo[1]])
             i = i+1
         np.savetxt(output_pref, compare_out, fmt='%.4f')
     else:
