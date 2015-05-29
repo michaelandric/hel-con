6c9983dc1bd8c15fe1e410b79aadb9057f8587bc
Michael Andric <andric.michael@gmail.com>
2015-05-29 19:01:28 +0200

    init functions for graph_evals

---
+++ do_graph_evals.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 17:34:54 2015
+
+@author: andric
+"""
+
+import os
+import time
+import numpy as np
+import graph_evals as ge
+# import networkx as nx
+
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+if __name__ == '__main__':
+
+    os.chdir(os.environ['hel'])
+    print os.getcwd()
+
+    top_dir = '%s/graph_analyses' % os.environ['hel']
+    if not os.path.exists(top_dir):
+        os.makedirs(top_dir)
+
+    niter = 100
+    mod_loc = 'modularity'
+
+    subj_list = ['hel19']
+    for ss in subj_list:
+        graph_dir = os.path.join(top_dir, '%s/graphs' % ss)
+        if not os.path.exists(graph_dir):
+            os.makedirs(graph_dir)
+        mod_dir = os.path.join(top_dir, '%s/modularity' % ss)
+        if not os.path.exists(mod_dir):
+            os.makedirs(mod_dir)
+        random_dir = os.path.join(top_dir, '%s/random' % ss)
+        if not os.path.exists(random_dir):
+            os.makedirs(random_dir)
+        random_graph_dir = random_dir+'/graphs/'
+        if not os.path.exists(random_graph_dir):
+            os.makedirs(random_graph_dir)
+        rand_mod_dir = os.path.join(random_dir, mod_loc)
+        if not os.path.exists(rand_mod_dir):
+            os.makedirs(rand_mod_dir)
+        proc_dir = os.path.join(os.environ['hel'], ss, 'preprocessing')
+
+        for session in range(1, 3):
+            ts_name = 'task_sess_%d_%s.txt' % (session, ss)
+            ts_file = os.path.join(proc_dir, ts_name)
+            n_nodes = file_len(ts_file)
+
+            for thresh_dens in np.arange(.05, .11, .1):
+                print 'Thresh: %s' % thresh_dens
+                print time.ctime()
+                graph_outname = 'task_sess_%d_%s.dens_%s.edgelist.gz' % \
+                    (session, ss, thresh_dens)
+                gr = ge.GRAPHS(ss, ts_file,
+                               thresh_dens, graph_dir,
+                               os.path.join(graph_dir, graph_outname))
+
+                # making graph:
+                print 'Making graph... '+time.ctime()
+                avg_r = np.zeros(1)
+                avg_r[0] = gr.make_graph()
+                avg_r_val_name = 'Avg_rval_task_sess_%d_%s.dens_%s.txt' % \
+                    (session, ss, thresh_dens)
+                avg_r_out = os.path.join(graph_dir, avg_r_val_name)
+                np.savetxt(avg_r_out, avg_r, fmt='%.4f')
+
+                # get modularity and trees
+                print 'Doing modularity evaluation... '
+                print time.ctime()
+                g = gr.make_networkx_graph(n_nodes)
+
+                Qs = np.zeros(niter)
+                trees = np.zeros(n_nodes*niter).reshape(n_nodes, niter)
+                for i in xrange(niter):
+                    trees[:, i], Qs[i] = gr.get_modularity(g)
+                Qs_outname = 'task_sess_%d_%s.dens_%s.Qval' % \
+                    (session, ss, thresh_dens)
+                np.savetxt(os.path.join(mod_dir, Qs_outname), Qs, fmt='%.4f')
+                trees_outname = 'task_sess_%d_%s.dens_%s.trees' % \
+                    (session, ss, thresh_dens)
+                np.savetxt(os.path.join(mod_dir, trees_outname),
+                           trees, fmt='%i')
---
+++ graph_evals.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 29 16:29:43 2015
+
+@author: andric
+"""
+
+import os
+import time
+import numpy as np
+import networkx as nx
+import bct
+from sklearn.metrics import normalized_mutual_info_score
+from sklearn.metrics import adjusted_rand_score
+
+
+class GRAPHS:
+
+    def __init__(self, subjid, inputTS, thresh_density,
+                 graph_dir, edgelist_name):
+        """
+        Initialize for hel
+        :param subjid: subject identifier
+        :param inputTS: the time series for input
+        :param thresh_density: This is the threshold
+        :return:
+        """
+        self.ss = subjid
+        self.dens = float(thresh_density)
+        self.input = inputTS
+        self.graph_dir = graph_dir
+        self.el_name = edgelist_name
+        print 'Initializing. -- '+time.ctime()
+
+    def make_graph(self):
+        """
+        Makes graph by pairwise correlation
+        Threshold at density
+        Returns average correlation among values
+        """
+        print 'Now making graph -- '+time.ctime()
+        ts = self.input.T   # this need to transpose
+        n_vox = ts.shape[0]
+        compl_graph = int((n_vox*(n_vox-1))/2)
+        n_edges = int(compl_graph*self.dens)
+        print 'Input is read. '
+        print 'Now getting the correlation matrix. '+time.ctime()
+        corrmat = np.corrcoef(ts)
+        corrmat_ut = np.nan_to_num(np.triu(corrmat, k=1))
+        print 'Starting sort. -- '+time.ctime()
+        corrsrtd = np.sort(corrmat_ut[corrmat_ut > 0], kind='mergesort')
+        print 'Sort done. \nThresholding... -- '+time.ctime()
+        threshd = corrsrtd[-n_edges:]
+        print 'Thresholding done. \nNow getting edge indices -- '+time.ctime()
+        ix = np.searchsorted(threshd, corrmat_ut, 'right')
+        n, v = np.where(ix)
+        inds = np.array(zip(n, v), dtype=np.int32)
+        print 'Got graph edges. \nWriting it to file -- '+time.ctime()
+        np.savetxt(self.el_name, inds, fmt='%d')
+        print 'Graph edgelist written out. \nDONE. -- '+time.ctime()
+        return np.mean(threshd)
+
+    def make_networkx_graph(self, n_nodes):
+        # setting it up for further use
+        g = nx.Graph()
+        g.add_nodes_from(range(148))
+        ed = nx.read_edgelist(os.path.join(self.graph_dir, self.el_name),
+                              nodetype=int)
+        g.add_edges_from(ed.edges())
+        return g
+
+    def get_modularity(self, graph):
+        """
+        Gets modularity louvain
+        Uses bct package
+        Returns communities and modularity value
+        at highest level
+        :param graph: Input graph via networkx
+        :return c: communities
+        :return q: modularity value
+        """
+        c, q = bct.modularity_louvain_und(nx.adjacency_matrix(graph).toarray())
+        return (c, q)
+
+    def max_q(self, fname):
+        """
+        Get the maximum modularity value
+        :param fname: File name containing Q values
+        :return : max q value, iteration with max q value
+        """
+        print 'Getting max q value -- %s' % time.ctime()
+        q_vals = np.loadtxt(fname)
+        iter_max = q_vals.argmax()
+        return (np.max(q_vals), iter_max)
+
+    def n_modules(self, tname):
+        """
+        Get the number of modules
+        :param tname: File name for tree
+        :return : number of modules > 1
+        """
+        from collections import Counter
+        print 'Getting number of modules -- %s' % time.ctime()
+        n_mods = np.zeros(100)
+        tree = np.loadtxt(tname)
+        cnts = np.array(Counter(tree[:, 1]).values())
+        n_mods = len(cnts[np.where(cnts > 1)])
+        return n_mods
+
+
+def adj_rand(p1, p2):
+    """
+    Return the Adjusted Rand Index
+    across two partitions
+    :param p1: partition 1
+    :param p2; partition 2
+    :return : Adjusted Rand Score
+    """
+    if len(p1) != len(p2):
+        print 'Subject needs a fix'
+        if len(p1) < len(p2):
+            p1 = np.append(p1, p1[len(p1)-1])
+        elif len(p2) < len(p1):
+            p2 = np.append(p2, p2[len(p2)-1])
+
+    ari = adjusted_rand_score(p1, p2)
+    return ari
+
+
+def normalized_MI(p1, p2):
+    """
+    Return the normalized mutual information
+    across two partitions
+    :param p1: partition 1
+    :param p2; partition 2
+    :return : normalized mutual information score
+    """
+    if len(p1) != len(p2):
+        print 'Subject needs a fix'
+        if len(p1) < len(p2):
+            p1 = np.append(p1, p1[len(p1)-1])
+        elif len(p2) < len(p1):
+            p2 = np.append(p2, p2[len(p2)-1])
+
+    nmi = normalized_mutual_info_score(p1, p2)
+    return nmi
