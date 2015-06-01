9f5513a2de2ccd77ba7640268845462f5e60d024
Michael Andric <andric.michael@gmail.com>
2015-06-01 18:02:44 +0200

    fix do_avg_global_conn
    removed thresh_dens bcs this isn’t a factor here
    had been looping through for no reason

--- do_avg_global_conn.py
+++ do_avg_global_conn.py
@@ -19,16 +19,15 @@ if __name__ == '__main__':
 
     top_dir = '%s/graph_analyses' % os.environ['hel']
 
-    for thresh_dens in [.05, .10, .15, .20]:
-        for ss in subj_list:
-            proc_dir = os.path.join(os.environ['hel'], ss, 'preprocessing')
-            conn_dir = os.path.join(top_dir, '%s/global_connectivity' % ss)
-            if not os.path.exists(conn_dir):
-                os.makedirs(conn_dir)
-            for session in range(1, 3):
-                ts_name = os.path.join(proc_dir,
-                                       'task_sess_%d_%s.txt' % (session, ss))
-                agc = ge.avg_global_connectivity(ts_name)
-                out_name = 'avg_corrZ_task_sess_%d_%s' % (session, ss)
-                out_fname = os.path.join(conn_dir, out_name)
-                np.savetxt(out_fname, agc, fmt='%.4f')
+    for ss in subj_list:
+        proc_dir = os.path.join(os.environ['hel'], ss, 'preprocessing')
+        conn_dir = os.path.join(top_dir, '%s/global_connectivity' % ss)
+        if not os.path.exists(conn_dir):
+            os.makedirs(conn_dir)
+        for session in range(1, 3):
+            ts_name = os.path.join(proc_dir,
+                                   'task_sess_%d_%s.txt' % (session, ss))
+            agc = ge.avg_global_connectivity(ts_name)
+            out_name = 'avg_corrZ_task_sess_%d_%s' % (session, ss)
+            out_fname = os.path.join(conn_dir, out_name)
+            np.savetxt(out_fname, agc, fmt='%.4f')
