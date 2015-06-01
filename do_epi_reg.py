32b9d81e5c5d66bebcef3bfa56b65f5907a444d4
Michael Andric <andric.michael@gmail.com>
2015-06-01 15:16:04 +0200

    init epi_reg
    test functions to do epi_reg, making template for later registrations

---
+++ do_epi_reg.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 14:01:31 2015
+
+@author: andric
+"""
+
+import os
+import general_procedures as gp
+
+
+if __name__ == '__main__':
+    subj_list = []
+    for i in range(1, 20):
+        subj_list.append('hel%d' % i)
+    subj_list.remove('hel9')   # because this is bad subj
+
+#    for ss in subj_list:
+    for ss in ['hel19']:
+        proc_dir = os.path.join(os.environ['hel'], ss, 'preprocessing')
+        epi_brain = "'%s/pb02_%sr06_ricorTS+orig[241]'" % (proc_dir, ss)
+        epi_nii_pref = '%s/pb02_%s_regslice' % (proc_dir, ss)
+        gp.converttoNIFTI(proc_dir, epi_brain, epi_nii_pref)
+
+        epi = '%s.nii.gz' % epi_nii_pref
+        anat_dir = os.path.join(os.environ['hel'], 'volume.%s.anat' % ss)
+        wholet1 = os.path.join(anat_dir, 'T1_biascorr.nii.gz' % ss)
+        extrt1 = os.path.join(anat_dir, 'T1_biascorr_brain.nii.gz' % ss)
+        epi_reg_out = os.path.join(anat_dir, 'epi2anat_%s_reg' % ss)
+        gp.epi_reg(ss, anat_dir, epi, wholet1, extrt1, epi_reg_out)
---
+++ general_procedures.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 13:06:28 2015
+
+@author: andric
+"""
+
+import os
+import time
+import numpy as np
+from shlex import split
+from subprocess import call, STDOUT
+
+
+def undump(subjid, ijk_coords, datafilename, data_dir, master_file):
+    """
+    :param subjid: Subject identifier
+    :param ijk_coords: IJK coordinates. GIVE FULL PATH
+    :param datafilename: name of the data file that you want to undump
+    :param data_dir: File where the data live
+    :param master_file: The master file for AFNI voxel resolution.
+    GIVE FULL PATH
+    Writes AFNI format Undumped file
+    """
+    print 'Doing undump... First pasting ijk to data'
+    print time.ctime()
+    ijkfile = np.loadtxt(ijk_coords)
+    data = np.loadtxt(datafilename)
+    data_ijk_outname = '%s.ijk' % datafilename
+    np.savetxt(data_ijk_outname, np.column_stack([ijkfile, data]),
+               fmt='%i %i %i %.4f')
+
+    stdout_dir = os.path.join(data_dir, 'stdout_files')
+    if not os.path.exists(stdout_dir):
+        os.makedirs(stdout_dir)
+    print 'Doing 3dUndump... %s' % time.ctime()
+    f = open('%s/stdout_from_undump.txt' % stdout_dir, 'w')
+    cmdargs = split('3dUndump -prefix %s -ijk \
+                    -datum %s -master %s %s' %
+                    (data_ijk_outname, 'float',
+                     master_file, data_ijk_outname))
+    call(cmdargs, stdout=f, stderr=STDOUT)
+    f.close()
+    print 'Finished undump '
+    print time.ctime()
+
+
+def avgepis(ss, epi_list, work_dir, outpref):
+    """
+    Make average TS brain
+    :param ss: Subject identifier
+    :param epi_list: Include list of epis that you average over
+    Writes to file a TS average brain
+    """
+    print 'Doing avgepis for %s -- ' % ss+time.ctime()
+    stdout_dir = os.path.join(work_dir, 'stdout_files')
+    if not os.path.exists(stdout_dir):
+        os.makedirs(stdout_dir)
+    f = open('%s/stdout_from_avgepis.txt' % stdout_dir, 'w')
+    cmdargs = split('3dMean -prefix %s %s' % (outpref, epi_list))
+    call(cmdargs, stdout=f, stderr=STDOUT)
+    f.close()
+
+
+def mean_epi(ss, infile, work_dir, outpref):
+    """
+    Average across TS mean brain to get one mean image.
+    YOU SHOULD FIRST HAVE AN AVERAGE OF TS (MANY IMAGES)
+    THIS IS WHAT YOU MAKE MEAN (ONE IMAGE).
+    Serves registration purposes.
+    :param ss: Subject identifier
+    Writes to file AFNI mean brain (one image)
+    """
+    print 'Doing mean_epi for %s -- ' % ss
+    print time.ctime()
+    stdout_dir = os.path.join(work_dir, 'stdout_files')
+    if not os.path.exists(stdout_dir):
+        os.makedirs(stdout_dir)
+    f = open('%s/stdout_from_mean_epi.txt' % stdout_dir, 'w')
+    cmdargs = split('3dTstat -prefix %s -mean %s' % (outpref, infile))
+    call(cmdargs, stdout=f, stderr=STDOUT)
+    f.close()
+
+
+def converttoNIFTI(work_dir, brain, prefix=None):
+    """
+    convert AFNI file to NIFTI
+    """
+    print 'Doing converttoNIFTI for %s -- ' % brain+time.ctime()
+    stdout_dir = os.path.join(work_dir, 'stdout_files')
+    if not os.path.exists(stdout_dir):
+        os.makedirs(stdout_dir)
+    f = open('%s/stdout_from_converttoNIFTI' % stdout_dir, 'w')
+    if prefix is None:
+        cmdargs = split('3dAFNItoNIFTI %s' % brain)
+    elif prefix:
+        cmdargs = split('3dAFNItoNIFTI -prefix %s %s' % (prefix, brain))
+    call(cmdargs, stdout=f, stderr=STDOUT)
+    f.close()
+
+
+def epi_reg(ss, work_dir, epi, wholet1, extrt1, out):
+    """
+    Register epi to t1
+    """
+    print 'Doing epi_reg for %s -- ' % ss+time.ctime()
+    stdout_dir = os.path.join(work_dir, 'stdout_files')
+    if not os.path.exists(stdout_dir):
+        os.makedirs(stdout_dir)
+    f = open('%s/stdout_from_epireg.txt' % stdout_dir, 'w')
+    cmdargs = split('epi_reg --epi=%s --t1=%s --t1brain=%s --out=%s' %
+                    (epi, wholet1, extrt1, out))
+    call(cmdargs, stdout=f, stderr=STDOUT)
+    f.close()
+
+
+def applywarpFLIRT(ss, work_dir, input, extrt1, out, premat):
+    """
+    Warp via linear transformation via fsl FLIRT
+    """
+    print 'doing applywarpFLIRT for %s -- ' % ss+time.ctime()
+    stdout_dir = os.path.join(work_dir, 'stdout_files')
+    if not os.path.exists(stdout_dir):
+        os.makedirs(stdout_dir)
+    f = open('%s/stdout_from_applywarpFLIRT.txt' % stdout_dir, 'w')
+    cmdargs = split('applywarp -i %s -r %s -o %s --premat=%s' %
+                    (input, extrt1, out, premat))
+    call(cmdargs, stdout=f, stderr=STDOUT)
+    f.close()
+
+
+def applywarpFNIRT(ss, work_dir, input, out, coeff):
+    """
+    Warp via nonlinear transformation via fsl FNIRT
+    """
+    print 'Doing applywarpFNIRT for %s -- ' % ss+time.ctime()
+    stdout_dir = os.path.join(work_dir, 'stdout_files')
+    if not os.path.exists(stdout_dir):
+        os.makedirs(stdout_dir)
+    f = open('%s/stdout_from_applywarp.txt' % stdout_dir, 'w')
+    cmdargs = split('applywarp -i %s \
+                    -r %s/data/standard/MNI152_T1_2mm.nii.gz \
+                    -o %s -w %s' %
+                    (input, os.environ['FSLDIR'], out, coeff))
+    call(cmdargs, stdout=f, stderr=STDOUT)
+    f.close()
