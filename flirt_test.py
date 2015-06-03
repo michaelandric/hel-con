3ef6c9fae683490a8d5b922e9a115bfebf1166ab
Michael Andric <andric.michael@gmail.com>
2015-06-03 19:33:04 +0200

    try flirt without bbr

--- flirt_test.py
+++ flirt_test.py
@@ -20,11 +20,15 @@ extrt1 = os.path.join(anat_dir, 'T1_biascorr_brain.nii.gz')
 epi_brain = "'%s/pb02_trim_despiked_%sr03+orig.[241]'" % (proc_dir, ss)
 epi_nii_pref = '%s/pb02_%s_regslice' % (proc_dir, ss)
 epi = '%s.nii.gz' % epi_nii_pref
-epi_reg_out = os.path.join(anat_dir, 'flirt2_bbr_epi2anat_%s_reg' % ss)
+# epi_reg_out = os.path.join(anat_dir, 'flirt2_bbr_epi2anat_%s_reg' % ss)
+epi_reg_out = os.path.join(anat_dir, 'flirt3_nobbr_epi2anat_%s_reg' % ss)
 wm_edge = os.path.join(anat_dir, 'epi2anat_%s_reg_fast_wmedge.nii.gz' % ss)
 
 f = open('%s/stdout_from_flirt2_test.txt' % anat_dir, 'w')
-cmdargs = split('flirt -in %s -ref %s -cost bbr -wmseg %s \
+# cmdargs = split('flirt -in %s -ref %s -cost bbr -wmseg %s \
+#                 -o %s' %
+#                 (epi, extrt1, wm_edge, epi_reg_out))
+cmdargs = split('flirt -in %s -ref %s -wmseg %s \
                 -o %s' %
                 (epi, extrt1, wm_edge, epi_reg_out))
 call(cmdargs, stdout=f, stderr=STDOUT)
