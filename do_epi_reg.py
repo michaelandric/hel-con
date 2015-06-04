d4b2e14b8270e2d7c6b74146568afa94a7fab1d0
Michael Andric <andric.michael@gmail.com>
2015-06-04 17:09:57 +0200

    registering best partition test with hel19

--- do_epi_reg.py
+++ do_epi_reg.py
@@ -21,9 +21,9 @@ if __name__ == '__main__':
     """
 
     top_dir = '%s/graph_analyses' % os.environ['hel']
-    for ss in subj_list:
+    for ss in ['hel19']:
         proc_dir = os.path.join(os.environ['hel'], ss, 'preprocessing')
-        conn_dir = os.path.join(top_dir, '%s/global_connectivity' % ss)
+        mod_dir = os.path.join(top_dir, '%s/modularity' % ss)
         vol_dir_pref = '%s/volume.%s.anat' % (ss, ss)
         anat_dir = os.path.join(os.environ['hel'], vol_dir_pref)
         wholet1 = os.path.join(anat_dir, 'T1_biascorr.nii.gz')
@@ -40,17 +40,19 @@ if __name__ == '__main__':
 #        gp.flirt_solo(anat_dir, epi, extrt1, wm_edge, epi_reg_out)
 
         for session in range(1, 3):
-            epi_nii_pref = '%s/avg_corrZ_task_sess_%d_%s.ijk' % \
-                (conn_dir, session, ss)
-            epi_in_pref = '%s+orig' % epi_nii_pref
-#            gp.converttoNIFTI(conn_dir, epi_in_pref, epi_nii_pref)
+            for thresh_dens in [.05, .10, .15, .20]:
+                epi_nii_pref = '%s/task_sess_%d_%s.dens_%s.maxq_tree.ijk' % \
+                    (mod_dir, session, ss, thresh_dens)
+                epi_in_pref = '%s+orig' % epi_nii_pref
+                gp.converttoNIFTI(mod_dir, epi_in_pref, epi_nii_pref)
 
-            in_fl = '%s.nii.gz' % epi_nii_pref
-            out_fl = '%s_flirted' % (epi_nii_pref)
-            gp.flirt_solo(anat_dir, in_fl, extrt1, wm_edge, out_fl)
+                in_fl = '%s.nii.gz' % epi_nii_pref
+                out_fl = '%s_flirted' % (epi_nii_pref)
+                gp.flirt_solo(anat_dir, in_fl, extrt1, wm_edge, out_fl)
 
-            in_fn = '%s.nii.gz' % out_fl
-            out_fn = '%s_fnirted_MNI2mm' % (epi_nii_pref)
-            fn_coef = os.path.join(anat_dir, 'T1_to_MNI_nonlin_coeff.nii.gz')
-            gp.applywarpFNIRT(ss, anat_dir, in_fn,
-                              out_fn, fn_coef)
+                in_fn = '%s.nii.gz' % out_fl
+                out_fn = '%s_fnirted_MNI2mm' % (epi_nii_pref)
+                fn_coef = os.path.join(anat_dir,
+                                       'T1_to_MNI_nonlin_coeff.nii.gz')
+                gp.applywarpFNIRT(ss, anat_dir, in_fn,
+                                  out_fn, fn_coef)
