cfb3b7ffead5b5f8072cda04eddc2e0aa1bb01ef
Michael Andric <andric.michael@gmail.com>
2015-06-05 15:23:03 +0200

    need to run on hel19 too

--- do_GMmask_group.py
+++ do_GMmask_group.py
@@ -20,26 +20,26 @@ if __name__ == '__main__':
         os.makedirs(grp_dir)
 
     subj_masks = []
-    for ss in subj_list:
+    for ss in ['hel19']:
         vol_dir_pref = '%s/volume.%s.anat' % (ss, ss)
         anat_dir = os.path.join(os.environ['hel'], vol_dir_pref)
         epi_nii_pref = os.path.join(anat_dir, '%s_GMmask' % ss)
         epi_in_pref = '%s+orig' % epi_nii_pref
-#        gp.converttoNIFTI(anat_dir, epi_in_pref, epi_nii_pref)
+        gp.converttoNIFTI(anat_dir, epi_in_pref, epi_nii_pref)
 
         in_fn = '%s.nii.gz' % epi_nii_pref
         out_fn = '%s_fnirted_MNI2mm' % (epi_nii_pref)
         fn_coef = os.path.join(anat_dir,
                                'T1_to_MNI_nonlin_coeff.nii.gz')
-#        gp.applywarpFNIRT(ss, anat_dir, in_fn, out_fn, fn_coef, 'nn')
+        gp.applywarpFNIRT(ss, anat_dir, in_fn, out_fn, fn_coef, 'nn')
 
-        ss_msk = '%s_GMmask_fnirted_MNI2mm.nii.gz' % ss
-        subj_masks.append(os.path.join(anat_dir, ss_msk))
+#        ss_msk = '%s_GMmask_fnirted_MNI2mm.nii.gz' % ss
+#        subj_masks.append(os.path.join(anat_dir, ss_msk))
     subj_masks_strlist = ' '.join(subj_masks)
     grp_msk_pref = 'group_avg_GMmask_fnirted_MNI2mm'
     grp_msk_fname = os.path.join(grp_dir, '%s.nii.gz' % grp_msk_pref)
-    gp.avgepis('group', subj_masks_strlist, grp_dir, grp_msk_fname)
+#    gp.avgepis('group', subj_masks_strlist, grp_dir, grp_msk_fname)
 
     out_frac_pref = '%s_frac.nii.gz' % os.path.join(grp_dir, grp_msk_pref)
     template = '%s/data/standard/MNI152_T1_2mm.nii.gz' % os.environ['FSLDIR']
-    gp.fractionize(grp_dir, grp_msk_fname, out_frac_pref, template)
+#    gp.fractionize(grp_dir, grp_msk_fname, out_frac_pref, template)
