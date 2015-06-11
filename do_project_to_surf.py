7f20fe7a8ae24347edc640555b2b2c8381d29352
Michael Andric <andric.michael@gmail.com>
2015-06-11 13:24:12 +0200

    init do_project_to_surf code

---
+++ do_project_to_surf.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 11 12:59:05 2015
+
+@author: andric
+"""
+
+import os
+import general_procedures as gp
+
+top_dir = '%s/graph_analyses' % os.environ['hel']
+group_dir = os.path.join(top_dir, 'group_jaccard')
+
+if __name__ == '__main__':
+
+    pn = '1.0'
+    for thresh_dens in [.05]:
+        for hemi in ['lh', 'rh']:
+            print 'Doing %s %s' % (hemi, thresh_dens)
+            parent_pref = os.path.join(group_dir,
+                                       'jaccard_group_median_dens_%s.ijk' % thresh_dens)
+            outname = '%s_%s_pn%s_MNI_N27.1D' % (parent_pref, hemi, pn)
+            gp.vol2surf_mni(hemi, '%s+tlrc' % parent_pref, pn, outname)
