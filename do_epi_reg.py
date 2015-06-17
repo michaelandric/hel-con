7604b83b1b3154883242a4c1cbc0a864b459261c
Michael Andric <andric.michael@gmail.com>
2015-06-17 12:30:08 +0200

    align gm_mask to group space 2mm MNI

--- do_epi_reg.py
+++ do_epi_reg.py
@@ -19,14 +19,13 @@ if __name__ == '__main__':
     interpol = 'nn'
     for ss in subj_list:
         proc_dir = os.path.join(os.environ['hel'], ss, 'preprocessing')
-        mod_dir = os.path.join(top_dir, '%s/modularity' % ss)
+#        mod_dir = os.path.join(top_dir, '%s/modularity' % ss)
         vol_dir_pref = '%s/volume.%s.anat' % (ss, ss)
         anat_dir = os.path.join(os.environ['hel'], vol_dir_pref)
         wholet1 = os.path.join(anat_dir, 'T1_biascorr.nii.gz')
         extrt1 = os.path.join(anat_dir, 'T1_biascorr_brain.nii.gz')
-#        gp.converttoNIFTI(proc_dir, epi_brain, epi_nii_pref)
 
-        premat = os.path.join(anat_dir, '%s_GMmask_frac_bin_flirted.mat' % ss)
+        """premat = os.path.join(anat_dir, '%s_GMmask_frac_bin_flirted.mat' % ss)
         for session in range(1, 3):
             for thresh_dens in [.05, .10, .15, .20]:
                 epi_nii_pref = '%s/task_sess_%d_%s.dens_%s.maxq_tree.ijk' % \
@@ -41,3 +40,19 @@ if __name__ == '__main__':
                 out_fn = '%s_fnirted_MNI2mm' % (epi_nii_pref)
                 gp.applywarpFNIRT(ss, mod_dir, in_fn, out_fn,
                                   fn_coef, interpol)
+        """
+
+        wm_edge = os.path.join(anat_dir,
+                               'epi2anat_%s_reg_fast_wmedge.nii.gz' % ss)
+        epi_nii_pref = '%s/%s_gm_mask_frac_bin' % (anat_dir, ss)
+        in_fl = '%s.nii.gz' % epi_nii_pref
+        out_fl = '%s_flirted' % (epi_nii_pref)
+        gp.flirt_solo(anat_dir, in_fl, extrt1, wm_edge,
+                      out_fl, 'nearestneighbour')
+
+        in_fn = '%s.nii.gz' % out_fl
+        out_fn = '%s_fnirted_MNI2mm' % (epi_nii_pref)
+        fn_coef = os.path.join(anat_dir,
+                               'T1_to_MNI_nonlin_coeff.nii.gz')
+        gp.applywarpFNIRT(ss, anat_dir, in_fn,
+                          out_fn, fn_coef, 'nn')
