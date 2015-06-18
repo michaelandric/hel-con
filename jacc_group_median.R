01d0419bead0d0564dc224dc8da46eefaf2f64dc
Michael Andric <andric.michael@gmail.com>
2015-06-18 13:46:46 +0200

    init jacc_group_median.R

---
+++ jacc_group_median.R
@@ -0,0 +1,30 @@
+# get the group median from mesh140
+library(dplyr)
+subjects <- c()
+s_nums = seq(1, 19)[seq(1,19) != 9]
+for (s in s_nums)
+{
+    subjects <- c(subjects, paste('hel', s, sep=''))
+}
+graph_dir <- paste('/cnari/normal_language/HEL/graph_analyses/', sep='')
+grp_jacc_dir <- paste(graph_dir, 'group_jaccard/', sep='')
+nnodes <- 196002
+
+for (td in seq(.05, .2, .05))
+{
+    for (h in c('lh', 'rh'))
+    {
+        group_mat <- matrix(ncol=length(subjects), nrow=nnodes)
+        for (i in 1:length(subjects))
+        {
+            ss <- subjects[i]
+            jacc_dir <- paste(graph_dir, ss, '/jaccard_res/', sep='')
+            fname <- paste('jacc_',ss,'_',td,'.ijk_',h,'_mesh140.1D', sep='')
+            group_mat[, i] <- read.table(paste(jacc_dir, fname, sep=''))$V2
+        }
+        grp_med <- apply(group_mat, 1, median)
+        grp_med_out <- cbind(seq(0, nnodes-1), grp_med)
+        outname <- paste(grp_jacc_dir, 'group_jaccard_median', h,'_mesh140_', td, '.1D', sep='')
+        write.table(grp_med_out, outname, row.names=FALSE, col.names=FALSE, quote=FALSE)
+    }
+}
