006affcdc5a187017571dff410854fc7d9c599df
Michael Andric <andric.michael@gmail.com>
2015-06-19 17:21:01 +0200

    init stratify and update get_agreement_mod

--- get_agreement_mod.m
+++ get_agreement_mod.m
@@ -1,6 +1,42 @@
+% Derive consensus graph
+% first permute agreement to see what you
+% get by chance
+% then write out the edgelist for the graph
+
 path(path,'/home/andric/BCT2015');
-M = dlmread('group_task_sess_1.dens_0.05.maxq_tree.ijk_fnirted_MNI2mm.txt');
+cd('/cnari/normal_language/HEL/graph_analyses/group_modularity');
+M = dlmread('group_task_sess_1.dens_0.05.maxq_tree.ijk_fnirted_MNI4mm.txt');
 M = M+1;
 
+[m, n] = size(M);
 
-gr = np.loadtxt('group_task_sess_1.dens_0.05.maxq_tree.ijk_fnirted_MNI2mm.txt')
+n_perms = 100
+perm_vec = zeros(n_perms, 1);
+for ii = 1:n_perms
+    rand_mat = zeros(m, n);
+    for i = 1:n
+        idx = randperm(m);
+        rand_mat(:, i) = M(idx, i);
+    end
+    ag_r = agreement(rand_mat);
+    perm_vec(ii) = max(ag_r(:));
+end
+
+% ff = find(m_ag > 14);
+% m_ag(ff) = 0;
+
+m_ag = agreement(M);
+[r, c] = find(m_ag > max(perm_vec));
+
+fileID = fopen('group_task_sess_1.dens_0.05.agreement.edgelist', 'w');
+for k = 1:length(r)
+    fprintf(fileID, '%d %d\n', r(k), c(k));
+end
+fclose(fileID);
+
+% binary format
+%fileID = fopen('group_task_sess_1.dens_0.05.agreement.edgelist.bin', 'w');
+%fwrite(fileID, [r, c], 'integer*4');
+%fclose(fileID);
+
+% [mod, q] = community_louvain(m_ag);
---
+++ stratify.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 12:30:50 2015
+
+Assign bin identifiers to SNSC values
+
+@author: andric
+"""
+
+
+import os
+from itertools import dropwhile
+import numpy as np
+
+
+def iscomment(s):
+    return s.startswith('#')
+
+
+def assigner(inputname, outname):
+    vertvals = []
+    f = open(inputname, "r")
+
+    for line in dropwhile(iscomment, f):
+        vertvals.append(line.split()[1])
+
+    vertvals = map(float, vertvals)
+
+    binvals = np.zeros(len(vertvals))
+
+    for i, v in enumerate(vertvals):
+        if v == 777:
+            binvals[i] = 4
+        elif v < .4:
+            binvals[i] = 5
+        elif v > .4 and v <= .45:
+            binvals[i] = 6
+        elif v > .45 and v <= .5:
+            binvals[i] = 7
+        elif v > .5 and v <= .55:
+            binvals[i] = 8
+        elif v > .55 and v <= .6:
+            binvals[i] = 9
+        elif v > .6:
+            binvals[i] = 10
+
+    binvals = map(int, binvals)
+
+    outdat = ''
+    for i, v in enumerate(binvals):
+        outdat += str(i)+' '+str(v)+'\n'
+
+    outf = open(outname, 'w')
+    outf.write(outdat)
+    outf.close()
+
+
+if __name__ == "__main__":
+
+    os.chdir('/cnari/normal_language/HEL/graph_analyses/group_jaccard')
+    for h in ['lh', 'rh']:
+        inname = 'group_jaccard_median_%s_mesh140_0.05.1D' % h
+        outname = 'group_jaccard_median_%s_mesh140_0.05.stratified.1D' % h
+        assigner(inname, outname)
