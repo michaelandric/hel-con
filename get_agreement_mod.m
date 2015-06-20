6ec42c10eedd6469684ac9541ddcdac125512e25
Michael Andric <andric.michael@gmail.com>
2015-06-20 11:30:48 +0200

    loop get_agreement mod for session

--- get_agreement_mod.m
+++ get_agreement_mod.m
@@ -5,60 +5,60 @@
 
 path(path,'/home/andric/BCT2015');
 cd('/cnari/normal_language/HEL/graph_analyses/group_modularity');
-session = 2
-td = .05
+for session = 1:2
+    td = .1
 
-fname = sprintf('group_task_sess_%d.dens_%g.maxq_tree.ijk_fnirted_MNI4mm.txt', session, td)
-M = dlmread(fname);
-M = M+1;
+    fname = sprintf('group_task_sess_%d.dens_%g.maxq_tree.ijk_fnirted_MNI4mm.txt', session, td)
+    M = dlmread(fname);
+    M = M+1;
 
-[m, n] = size(M);
+    [m, n] = size(M);
 
-n_perms = 100
-perm_vec = zeros(n_perms, 1);
-for ii = 1:n_perms
-    rand_mat = zeros(m, n);
-    for i = 1:n
-        idx = randperm(m);
-        rand_mat(:, i) = M(idx, i);
+    n_perms = 100
+    perm_vec = zeros(n_perms, 1);
+    for ii = 1:n_perms
+        rand_mat = zeros(m, n);
+        for i = 1:n
+            idx = randperm(m);
+            rand_mat(:, i) = M(idx, i);
+        end
+        ag_r = agreement(rand_mat);
+        perm_vec(ii) = max(ag_r(:));
     end
-    ag_r = agreement(rand_mat);
-    perm_vec(ii) = max(ag_r(:));
+
+    pv_fname = sprintf('group_task_sess_%d.dens_%g.agreement.perm_vec', session, td)
+    perm_vec_file = fopen(pv_fname, 'W')
+    for p = 1:n_perms
+        fprintf(perm_vec_file, '%d\n', perm_vec(p));
+    end
+    fclose(perm_vec_file);
+
+    m_ag = agreement(M);
+    [mod, q] = community_louvain(m_ag);
+    mod_fname = sprintf('group_task_sess_%d.dens_%g.agreement.nothr.mods', session, td)
+    modfile = fopen(mod_fname, 'w');
+    for m = 1:length(mod)
+        fprintf(modfile, '%d\n', mod(m));
+    end
+    fclose(modfile);
+    q_fname = sprintf('group_task_sess_%d.dens_%g.agreement.nothr.Qval', session, td)
+    qfile = fopen(q_fname, 'w');
+    fprintf(qfile, '%f\n', q);
+    fclose(qfile);
+
+
+    % 14 is the value at 95/100 for session 1
+    srt_pv = sort(perm_vec);
+    thr = srt_pv(95);
+    [r, c] = find(m_ag > thr);
+    length(r)
+    el_fname = sprintf('group_task_sess_%d.dens_%g.agreement.thr.edgelist', session, td)
+    fileID = fopen(el_fname, 'w');
+    for k = 1:length(r)
+        fprintf(fileID, '%d %d\n', r(k), c(k));
+    end
+    fclose(fileID);
 end
-
-pv_fname = sprintf('group_task_sess_%d.dens_%g.agreement.perm_vec', session, td)
-perm_vec_file = fopen(pv_fname, 'W')
-for p = 1:n_perms
-    fprintf(perm_vec_file, '%d\n', perm_vec(p));
-end
-fclose(perm_vec_file);
-
-m_ag = agreement(M);
-[mod, q] = community_louvain(m_ag);
-mod_fname = sprintf('group_task_sess_%d.dens_%g.agreement.nothr.mods', session, td)
-modfile = fopen(mod_fname, 'w');
-for m = 1:length(mod)
-    fprintf(modfile, '%d\n', mod(m));
-end
-fclose(modfile);
-q_fname = sprintf('group_task_sess_%d.dens_%g.agreement.nothr.Qval', session, td)
-qfile = fopen(q_fname, 'w');
-fprintf(qfile, '%f\n', q);
-fclose(qfile);
-
-
-% 14 is the value at 95/100 for session 1
-srt_pv = sort(perm_vec);
-thr = srt_pv(95);
-[r, c] = find(m_ag > thr);
-length(r)
-el_fname = sprintf('group_task_sess_%d.dens_%g.agreement.thr.edgelist', session, td)
-fileID = fopen(el_fname, 'w');
-for k = 1:length(r)
-    fprintf(fileID, '%d %d\n', r(k), c(k));
-end
-fclose(fileID);
-
 
 
 
