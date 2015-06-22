89969c9f914c9a86800556818b3214c0a3c59dfe
Michael Andric <andric.michael@gmail.com>
2015-06-22 13:01:39 +0200

    added section to do 2sess
    “2sess” is the agreement across both sessions

--- do_agree_mod_graph_evals.py
+++ do_agree_mod_graph_evals.py
@@ -22,7 +22,7 @@ if not os.path.exists(trees_all_levl_dir):
 
 n_nodes = 23155
 
-for thresh_dens in [.05, .10, .15, .20]:
+"""for thresh_dens in [.05, .10, .15, .20]:
     el_suff = 'dens_%s.agreement.thr.edgelist' % thresh_dens
     for session in range(1, 3):
         el_name = 'group_task_sess_%d.%s' % (session, el_suff)
@@ -58,4 +58,38 @@ for thresh_dens in [.05, .10, .15, .20]:
         np.savetxt(os.path.join(mod_dir, nmods_outname),
                    nmods, fmt='%i')
         bin_file = '%s.bin' % edg_list
-        os.remove(bin_file)
+        os.remove(bin_file)"""
+
+for thresh_dens in [.05, .10, .15, .20]:
+    el_suff = 'dens_%s.agreement.thr.edgelist' % thresh_dens
+
+    el_name = 'group_task_2sess_%s' % el_suff
+    edg_list = os.path.join(grp_conn_dir, el_name)
+    com = ge.CommunityDetect(edg_list)
+    com.convert_graph()
+
+    Qs = np.zeros(niter)
+    nmods = np.zeros(niter)
+    trees = np.zeros(n_nodes*niter).reshape(n_nodes, niter)
+    hierar_suff = 'group_task_2sess_dens_%s.trees_hierarchy' % thresh_dens
+    for i in xrange(niter):
+        print 'iter %d' % i
+        hierarchy_tr_name = 'iter%d.%s' % (i, hierar_suff)
+        hierarchy_tr_filename = os.path.join(trees_all_levl_dir,
+                                             hierarchy_tr_name)
+        Qs[i] = com.get_modularity(hierarchy_tr_filename)
+        tr, n_m = com.get_hierarchical(hierarchy_tr_filename)
+        if len(tr) == trees.shape[0]-1:
+            tr = np.append(tr, tr[len(tr)-1])
+        trees[:, i] = tr
+        nmods[i] = n_m
+    Qs_outname = 'task_2sess_dens_%s.Qval' % thresh_dens
+    np.savetxt(os.path.join(mod_dir, Qs_outname), Qs, fmt='%.4f')
+    trees_outname = 'task_2sess_dens_%s.trees' % thresh_dens
+    np.savetxt(os.path.join(mod_dir, trees_outname),
+               trees, fmt='%i')
+    nmods_outname = 'task_2sess_dens_%s.nmods' % thresh_dens
+    np.savetxt(os.path.join(mod_dir, nmods_outname),
+               nmods, fmt='%i')
+    bin_file = '%s.bin' % edg_list
+    os.remove(bin_file)
