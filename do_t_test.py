1d85255941b50a4dc2d91fc15c933636d927702a
Michael Andric <andric.michael@gmail.com>
2015-06-24 15:10:32 +0200

    added t_test for tcorr data

--- do_t_test.py
+++ do_t_test.py
@@ -35,18 +35,35 @@ def t_test(stdoutdir, ss_list, outpref):
     f.close()
 
 
+def tcorr_t_test(stdoutdir, ss_list, outpref):
+    """
+    Testing whether biaseless ISC values
+    differ from 0
+    """
+    a_set = []
+    tcorr_dir = os.path.join(os.environ['hel'], 'tcorr_group')
+    for ss in ss_list:
+        pref = 'tcorr_%s_gm_mskd_Z_fnirted_MNI2mm.nii.gz' % ss
+        a_set.append(os.path.join(tcorr_dir, pref))
+    a_set = ' '.join(a_set)
+
+    f = open('%s/stdout_from_3dttest++.txt' % stdoutdir, 'w')
+    cmdargs = split('3dttest++ -setA %s \
+                    -mask %s/MNI152_T1_2mm_brain_mask_dil1.nii.gz \
+                    -prefix %s' %
+                    (a_set, '%s/data/standard' % os.environ['FSLDIR'],
+                     outpref))
+    call(cmdargs, stdout=f, stderr=STDOUT)
+    f.close()
+
 if __name__ == '__main__':
     subj_list = []
     for i in range(1, 20):
         subj_list.append('hel%d' % i)
     subj_list.remove('hel9')   # because this is bad subj
-    out_dir = os.path.join(os.environ['hel'],
-                           'graph_analyses',
-                           'group_global_connectivity')
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
+    out_dir = os.path.join(os.environ['hel'], 'tcorr_group')
     stdout_dir = os.path.join(out_dir, 'stdout_files')
     if not os.path.exists(stdout_dir):
         os.makedirs(stdout_dir)
-    outpref = os.path.join(out_dir, 'ttest_avg_corrZ')
-    t_test(stdout_dir, subj_list, outpref)
+    outpref = os.path.join(out_dir, 'ttest_tcorr_Z')
+    tcorr_t_test(stdout_dir, subj_list, outpref)
