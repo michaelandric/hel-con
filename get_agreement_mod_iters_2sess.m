0ebb8db90fce7227ce2d77e36147d1e4dd3e87f2
Michael Andric <andric.michael@gmail.com>
2015-07-12 19:11:32 +0200

    cleaner code

--- get_agreement_mod_iters_2sess.m
+++ get_agreement_mod_iters_2sess.m
@@ -5,7 +5,8 @@
 path(path,'/home/andric/BCT2015');
 cd('/cnari/normal_language/HEL/graph_analyses/group_modularity_thr0.5msk');
 for td = [.05, .1, .15, .2]
-
+    disp(td);
+    disp(datestr(now))
     fname1 = sprintf('group_task_sess_%d.dens_%g.maxq_tree.ijk_fnirted_MNI4mm_thr0.5.txt', 1, td)
     M1 = dlmread(fname1);
     fname2 = sprintf('group_task_sess_%d.dens_%g.maxq_tree.ijk_fnirted_MNI4mm_thr0.5.txt', 2, td)
@@ -21,14 +22,10 @@ for td = [.05, .1, .15, .2]
     q_vec = zeros(n_perms, 1);
     mod_arr = zeros(m, n_perms);
     for ii = 1:n_perms
-        tic
-        disp(ii)
         [mod, q] = community_louvain(m_ag);
-        toc
         q_vec(ii) = q;
         mod_arr(:,ii) = mod;
     end
-
     mod_fname = sprintf('group_task_2sess_dens_%g.agreement.nothr.mod_arr', td)
     dlmwrite(mod_fname, mod_arr, ' ');
 
