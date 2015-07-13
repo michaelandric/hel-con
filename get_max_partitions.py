874b6e8ec3728bffa96de8825824973d3ce765c7
Michael Andric <andric.michael@gmail.com>
2015-07-14 01:01:18 +0200

    update get_max_partitions
    getting aggregate tree

--- get_max_partitions.py
+++ get_max_partitions.py
@@ -12,24 +12,23 @@ import numpy as np
 import graph_evals as ge
 
 grp_conn_dir = os.path.join(os.environ['hel'],
-                            'graph_analyses', 'group_modularity')
-mod_dir = os.path.join(grp_conn_dir, 'modularity_iters')
+                            'graph_analyses', 'group_modularity_thr0.5msk')
+mod_dir = grp_conn_dir
 
 if __name__ == '__main__':
-
-    subj_list = []
-    for i in range(1, 20):
-        subj_list.append('hel%d' % i)
-    subj_list.remove('hel9')   # because this is bad subj
 
     for thresh_dens in [.05, .10, .15, .20]:
         gr = ge.Graphs('group', 'agreement_stuff',
                        thresh_dens, grp_conn_dir)
-        q_fname = 'task_2sess_dens_%s.Qval' % thresh_dens
-        qval, iter_max = gr.max_q(os.path.join(mod_dir, q_fname))
-        trees_fname = 'task_2sess_dens_%s.trees' % thresh_dens
-        trees = np.loadtxt(os.path.join(mod_dir, trees_fname))
-        best_tree = trees[:, iter_max]+1
-        best_tree_fname = 'task_2sess_dens_%s.maxq_tree' % thresh_dens
-        np.savetxt(os.path.join(mod_dir, best_tree_fname),
-                   best_tree, fmt='%i')
+        for session in range(1, 3):
+            qsuff = 'agreement.nothr.Qval'
+            pref = 'group_task_sess_%d' % session
+            q_fname = '%s.dens_%s.%s' % (pref, thresh_dens, qsuff)
+            qval, iter_max = gr.max_q(os.path.join(mod_dir, q_fname))
+            tsuff = 'agreement.nothr.mod_arr'
+            trees_fname = '%s.dens_%s.%s' % (pref, thresh_dens, tsuff)
+            trees = np.loadtxt(os.path.join(mod_dir, trees_fname))
+            best_tree = trees[:, iter_max]+1
+            best_tree_fname = '%s.dens_%s.maxq_tree' % (pref, thresh_dens)
+            np.savetxt(os.path.join(mod_dir, best_tree_fname),
+                       best_tree, fmt='%i')
