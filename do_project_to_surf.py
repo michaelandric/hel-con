7465ee94e9292a8d787d96d72a0b4a084dd24f5a
Michael Andric <andric.michael@gmail.com>
2015-07-29 18:13:03 +0200

    projecting tcorr data to surface

--- do_project_to_surf.py
+++ do_project_to_surf.py
@@ -8,8 +8,9 @@ Created on Thu Jun 11 12:59:05 2015
 import os
 import general_procedures as gp
 
-graph_dir = os.path.join(os.environ['hel'], 'graph_analyses')
-mod_dir = os.path.join(graph_dir, 'group_modularity')
+# graph_dir = os.path.join(os.environ['hel'], 'graph_analyses')
+# mod_dir = os.path.join(graph_dir, 'group_modularity')
+tcorr_dir = os.path.join(os.environ['hel'], 'tcorr_group')
 
 if __name__ == '__main__':
 
@@ -18,13 +19,12 @@ if __name__ == '__main__':
         subj_list.append('hel%d' % i)
     subj_list.remove('hel9')   # because this is bad subj
 
-    pn = '2.0'
-    for thresh_dens in [.05, .10, .15, .20]:
-        for session in range(1, 3):
-            for hemi in ['lh', 'rh']:
-                print 'Doing %s ' % hemi
-                parent_pref = os.path.join(mod_dir,
-                                           'group_task_sess_%d.dens_%s.agreement.nothr.mods.ijk' % (session, thresh_dens))
-                outname = '%s_%s_pn%s_MNI_N27.1D' % (parent_pref, hemi, pn)
-                gp.vol2surf_mni(mod_dir, hemi, '%s+tlrc' % parent_pref,
-                                pn, outname)
+    pn = '1.0'
+
+    for hemi in ['lh', 'rh']:
+        print 'Doing %s ' % hemi
+        parent_pref = os.path.join(tcorr_dir,
+                                   'ttest_tcorr_Z_vals')
+        outname = '%s_%s_pn%s_MNI_N27.1D' % (parent_pref, hemi, pn)
+        gp.vol2surf_mni(tcorr_dir, hemi, '%s+tlrc' % parent_pref,
+                        pn, outname)
