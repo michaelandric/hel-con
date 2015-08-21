4919f947cb514ba52945a2b7ceb6387642c766ec
Michael Andric <andric.michael@gmail.com>
2015-08-21 19:20:02 +0200

    changes to do_make_grp_mat

--- do_make_grp_mat.py
+++ do_make_grp_mat.py
@@ -9,7 +9,7 @@ import os
 import numpy as np
 
 top_dir = '%s/graph_analyses' % os.environ['hel']
-nvox = 20071
+nvox = 262245
 suff = 'maxq_tree.ijk_fnirted_MNI4mm_thr0.5.txt'
 
 if __name__ == '__main__':
@@ -20,18 +20,14 @@ if __name__ == '__main__':
 
     top_dir = '%s/graph_analyses' % os.environ['hel']
 
-    for thresh_dens in [.05]:
-        for session in range(1, 3):
-            grp_mat = np.zeros(nvox*len(subj_list))
-            grp_mat = grp_mat.reshape(nvox, len(subj_list))
-            for i, ss in enumerate(subj_list):
-                mod_dir = os.path.join(top_dir, '%s/modularity' % ss)
-                f_name = 'task_sess_%s_%s.dens_%s.%s' % (session, ss,
-                                                         thresh_dens, suff)
-                in_fname = os.path.join(mod_dir, f_name)
-                grp_mat[:, i] = np.loadtxt(in_fname)
-            out_f_name = 'group_task_sess_%s.dens_%s.%s' % (session,
-                                                            thresh_dens, suff)
-            out_f = os.path.join(top_dir,
-                                 'group_modularity_thr0.5msk', out_f_name)
-            np.savetxt(out_f, grp_mat, fmt='%i')
+    grp_mat = np.zeros(nvox*len(subj_list))
+    grp_mat = grp_mat.reshape(nvox, len(subj_list))
+    for i, ss in enumerate(subj_list):
+        conn_dir = os.path.join(top_dir, '%s/global_connectivity' % ss)
+        f_name = '%s_Clust_msk_glob_conn_knnward_clst1.ijk_fnirted_MNI2mm.txt' % ss
+        in_fname = os.path.join(conn_dir, f_name)
+        grp_mat[:, i] = np.loadtxt(in_fname)
+    out_f_name = 'group_Clust_msk_glob_conn_knnward_clst1'
+    out_f = os.path.join(top_dir,
+                         'group_global_connectivity', out_f_name)
+    np.savetxt(out_f, grp_mat, fmt='%i')
