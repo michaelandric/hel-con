bb3eb679b82c6dab7c3e7b7c0b4245a86744d6cb
Michael Andric <andric.michael@gmail.com>
2015-10-06 16:28:20 +0200

    iterate through clusters 2 and 3 for do_avg_global_conn_withseed.py

--- do_avg_global_conn_withseed.py
+++ do_avg_global_conn_withseed.py
@@ -8,8 +8,6 @@ Created on Mon Sep 28 16:12:26 2015
 import os
 import numpy as np
 import time
-from shlex import split
-from subprocess import call, STDOUT
 
 
 def corr_coeff2(A, B):
@@ -45,36 +43,34 @@ def seed_global_connectivity(ts, seed, transform=True):
     else:
         return seed_corr
 
-
 if __name__ == '__main__':
     subj_list = []
     for i in range(1, 20):
         subj_list.append('hel%d' % i)
     subj_list.remove('hel9')   # because this is bad subj
 
-    outn_pref = 'knnward_clst1_mskd'
+    for cl in [2, 3]:
+        outn_pref = 'knnward_clst%d_mskd' % cl
 
-    for ss in ['hel19']:
-        ss_dir = os.path.join(os.environ['hel'], ss)
-        proc_dir = os.path.join(ss_dir, 'preprocessing')
-        anat_dir = os.path.join(ss_dir, 'volume.%s.anat' % ss)
-        conn_dir = os.path.join(os.environ['hel'], 'graph_analyses',
-                                '%s/global_connectivity' % ss)
-        maskf_name = '%s_consensus_prtn_knnward_clst1_mskd_origspace.txt' % ss
-        mask_file = os.path.join(anat_dir, maskf_name)
-        m = np.loadtxt(mask_file)
-        subclusters = np.unique(m[m != 0])
-        for session in range(1, 3):
-            ts_name = os.path.join(proc_dir,
-                                   'task_sess_%d_%s_gm_mskd.txt' %
-                                   (session, ss))
-            ts = np.loadtxt(ts_name)
-            for clst in subclusters:
-                seed = np.mean(ts[np.where(m == clst)[0], :], axis=0)
-                sgc = seed_global_connectivity(ts, seed)
-                outname = '%s_subclust%d_corrZ_sess_%d_%s' % \
-                          (outn_pref, clst, session, ss)
-                outfname = os.path.join(conn_dir, outname)
-                np.savetxt(outfname, sgc, fmt='%.4f')
-
-                
+        for ss in subj_list:
+            ss_dir = os.path.join(os.environ['hel'], ss)
+            proc_dir = os.path.join(ss_dir, 'preprocessing')
+            anat_dir = os.path.join(ss_dir, 'volume.%s.anat' % ss)
+            conn_dir = os.path.join(os.environ['hel'], 'graph_analyses',
+                                    '%s/global_connectivity' % ss)
+            maskf_name = '%s_consensus_prtn_knnward_clst%d_mskd_origspace.txt' % (ss, cl)
+            mask_file = os.path.join(anat_dir, maskf_name)
+            m = np.loadtxt(mask_file)
+            subclusters = np.unique(m[m != 0])
+            for session in range(1, 3):
+                ts_name = os.path.join(proc_dir,
+                                       'task_sess_%d_%s_gm_mskd.txt' %
+                                       (session, ss))
+                ts = np.loadtxt(ts_name)
+                for clst in subclusters:
+                    seed = np.mean(ts[np.where(m == clst)[0], :], axis=0)
+                    sgc = seed_global_connectivity(ts, seed)
+                    outname = '%s_subclust%d_corrZ_sess_%d_%s' % \
+                              (outn_pref, clst, session, ss)
+                    outfname = os.path.join(conn_dir, outname)
+                    np.savetxt(outfname, sgc, fmt='%.4f')
