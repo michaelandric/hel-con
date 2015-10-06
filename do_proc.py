9a8ad1a259689cfeedb5c054388e6a0c5f94149b
Michael Andric <andric.michael@gmail.com>
2015-10-06 16:48:45 +0200

    do_proc.py adjusted to undump subclusters 2 and 3
    for avg_global_connectivity analyses undumping subcultures from
    significant clusters 2 and 3

--- do_proc.py
+++ do_proc.py
@@ -6,6 +6,7 @@ Created on Mon Jun  1 16:55:40 2015
 """
 
 import os
+import numpy as np
 import general_procedures as gp
 
 
@@ -16,22 +17,27 @@ if __name__ == '__main__':
     subj_list.remove('hel9')   # because this is bad subj
 
     graph_dir = os.path.join(os.environ['hel'], 'graph_analyses')
-    for ss in subj_list:
-        vol_dir_pref = '%s/volume.%s.anat' % (ss, ss)
-        anat_dir = os.path.join(os.environ['hel'], vol_dir_pref)
-        conn_dir = os.path.join(graph_dir, '%s/global_connectivity' % ss)
-        st_odir = os.path.join(conn_dir, 'stdout_dir')
-        if not os.path.exists(st_odir):
-            os.makedirs(st_odir)
-        for session in range(1, 3):
-            # because there are 3 subclusters:
-            for clst in range(1, 4):
-                corr_z_pref = 'knnward_clst1_mskd_subclust%d_corrZ_sess_%s_%s' % \
-                    (clst, session, ss)
-                corr_z_fname = os.path.join(conn_dir, corr_z_pref)
-                ijk_name = os.path.join(anat_dir,
-                                        '%s_gm_mask_frac_bin_ijk.txt' % ss)
-                master_file = os.path.join(anat_dir,
-                                           '%s_gm_mask_frac_bin.nii.gz' % ss)
-                gp.undump(ss, ijk_name, corr_z_fname, conn_dir,
-                          master_file, 'float')
+    for cl in [2, 3]:
+        maskfname = 'consensus_prtn_knnward_clst%d_mskd' % cl
+        maskf = os.path.join(graph_dir, 'group_global_connectivity',
+                             maskfname)
+        msk = np.loadtxt(maskf)
+        subclusters = np.unique(msk[msk != 0])
+        for ss in subj_list:
+            vol_dir_pref = '%s/volume.%s.anat' % (ss, ss)
+            anat_dir = os.path.join(os.environ['hel'], vol_dir_pref)
+            conn_dir = os.path.join(graph_dir, '%s/global_connectivity' % ss)
+            st_odir = os.path.join(conn_dir, 'stdout_dir')
+            if not os.path.exists(st_odir):
+                os.makedirs(st_odir)
+            for session in range(1, 3):
+                for clst in subclusters:
+                    corr_z_pref = 'knnward_clst%d_mskd_subclust%d_corrZ_sess_%s_%s' % \
+                                  (cl, clst, session, ss)
+                    corr_z_fname = os.path.join(conn_dir, corr_z_pref)
+                    ijk_name = os.path.join(anat_dir,
+                                            '%s_gm_mask_frac_bin_ijk.txt' % ss)
+                    master_file = os.path.join(anat_dir,
+                                               '%s_gm_mask_frac_bin.nii.gz' % ss)
+                    gp.undump(ss, ijk_name, corr_z_fname, conn_dir,
+                              master_file, 'float')
