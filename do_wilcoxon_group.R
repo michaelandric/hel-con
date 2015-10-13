e8df734d2cdbd07ca50e0962f2c78531754ba8d0
Michael Andric <andric.michael@gmail.com>
2015-10-13 15:56:33 +0200

    updated do_wilcoxon_group.R to do the ccf_abs set

--- do_wilcoxon_group.R
+++ do_wilcoxon_group.R
@@ -1,5 +1,5 @@
 # Doing Wilcoxon signed-rank test
-setwd('/cnari/normal_language/HEL/graph_analyses/group_global_connectivity')
+setwd('/cnari/normal_language/HEL/ccf_cor')
 subjects <- c()
 s_nums = seq(1, 19)[seq(1,19) != 9]
 for (s in s_nums)
@@ -9,12 +9,10 @@ for (s in s_nums)
 
 nvox <- 262245
 group_mat <- matrix(ncol=length(subjects), nrow=nvox)
-graph_dir <- paste('/cnari/normal_language/HEL/graph_analyses/', sep='')
 lb <- 'lag'
 for (ss in 1:length(subjects))
 {
-    ssdir <- paste(graph_dir, subjects[ss], 'global_connectivity', sep='/')
-    group_mat[, ss] <- read.table(paste(ssdir, '/ccf_',lb,'_out_',subjects[ss],'_gm_mskd.ijk_fnirted_MNI2mm.txt', sep=''))$V1
+    group_mat[, ss] <- read.table(paste('ccf_abs_',lb,'_out_',subjects[ss],'_gm_mskd.ijk_fnirted_MNI2mm.txt', sep=''))$V1
 }
 
 oo <- apply(group_mat, 1, wilcox.test, correct=FALSE)
@@ -22,6 +20,6 @@ med_vals <- apply(group_mat, 1, median)
 pvals <- sapply(seq_along(1:nvox), function(i)oo[[i]]$p.value[[1]]) 
 stats <- sapply(seq_along(1:nvox), function(i)oo[[i]]$statistic[[1]]) 
 
-write.table(stats, paste('ccf_',lb,'_out_group_gm_mskd_wilcox_stats', sep=''), row.names=F, col.names=F, quote=F)
-write.table(pvals, paste('ccf_',lb,'_out_group_gm_mskd_wilcox_pvals', sep=''), row.names=F, col.names=F, quote=F)
-write.table(med_vals, paste('ccf_',lb,'_out_group_gm_mskd_median_vals', sep=''), row.names=F, col.names=F, quote=F)
+write.table(stats, paste('ccf_abs_',lb,'_out_group_gm_mskd_wilcox_stats', sep=''), row.names=F, col.names=F, quote=F)
+write.table(pvals, paste('ccf_abs_',lb,'_out_group_gm_mskd_wilcox_pvals', sep=''), row.names=F, col.names=F, quote=F)
+write.table(med_vals, paste('ccf_abs_',lb,'_out_group_gm_mskd_median_vals', sep=''), row.names=F, col.names=F, quote=F)
