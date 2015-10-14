ec93498cc8ed3bc7270dfa0ef740888ab96430f1
Michael Andric <andric.michael@gmail.com>
2015-10-14 16:07:39 +0200

    changed stratify to handle 10% density thresh
    Lowered stratify levels by .5 to accommodate 10% dens jaccard data

--- stratify.py
+++ stratify.py
@@ -31,17 +31,17 @@ def assigner(inputname, outname):
     for i, v in enumerate(vertvals):
         if v == 777:
             binvals[i] = 4
-        elif v < .4:
+        elif v < .35:
             binvals[i] = 5
-        elif v > .4 and v <= .45:
+        elif v > .35 and v <= .4:
             binvals[i] = 6
-        elif v > .45 and v <= .5:
+        elif v > .4 and v <= .45:
             binvals[i] = 7
-        elif v > .5 and v <= .55:
+        elif v > .45 and v <= .5:
             binvals[i] = 8
-        elif v > .55 and v <= .6:
+        elif v > .5 and v <= .55:
             binvals[i] = 9
-        elif v > .6:
+        elif v > .55:
             binvals[i] = 10
 
     binvals = map(int, binvals)
@@ -59,6 +59,6 @@ if __name__ == "__main__":
 
     os.chdir('/cnari/normal_language/HEL/graph_analyses/group_jaccard')
     for h in ['lh', 'rh']:
-        inname = 'group_jaccard_median_%s_mesh140_0.05.1D' % h
-        outname = 'group_jaccard_median_%s_mesh140_0.05.stratified.1D' % h
+        inname = 'group_jaccard_median_%s_mesh140_0.1.1D' % h
+        outname = 'group_jaccard_median_%s_mesh140_0.1.stratified.1D' % h
         assigner(inname, outname)
