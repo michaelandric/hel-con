81dd5ec923804f591c2bca4c58c90d521aac433f
Michael Andric <andric.michael@gmail.com>
2015-10-15 17:20:00 +0200

    adjusted stratify.py to do stratified_2
    “stratified_2” uses lower range values because only .05 surpassed .6
    jaccard coef

--- stratify.py
+++ stratify.py
@@ -18,6 +18,12 @@ def iscomment(s):
 
 
 def assigner(inputname, outname):
+    """
+    orig range is .4, .6
+    But only relevant for 5% dens thresh
+    Modified for 'stratified_2' with
+    lowered by .5, so range is .35, .55
+    """
     vertvals = []
     f = open(inputname, "r")
 
@@ -31,17 +37,17 @@ def assigner(inputname, outname):
     for i, v in enumerate(vertvals):
         if v == 777:
             binvals[i] = 4
-        elif v < .4:
+        elif v < .35:
             binvals[i] = 5
-        elif v > .4 and v <= .45:
+        elif v > .35 and v <= .4:
             binvals[i] = 6
-        elif v > .45 and v <= .5:
+        elif v > .4 and v <= .45:
             binvals[i] = 7
-        elif v > .5 and v <= .55:
+        elif v > .45 and v <= .5:
             binvals[i] = 8
-        elif v > .55 and v <= .6:
+        elif v > .5 and v <= .55:
             binvals[i] = 9
-        elif v > .6:
+        elif v > .55:
             binvals[i] = 10
 
     binvals = map(int, binvals)
@@ -59,6 +65,7 @@ if __name__ == "__main__":
 
     os.chdir('/cnari/normal_language/HEL/graph_analyses/group_jaccard')
     for h in ['lh', 'rh']:
-        inname = 'group_jaccard_median_%s_mesh140_0.1.1D' % h
-        outname = 'group_jaccard_median_%s_mesh140_0.1.stratified.1D' % h
-        assigner(inname, outname)
+        for td in [.05, .1]:
+            inname = 'group_jaccard_median_%s_mesh140_%s.1D' % (h, td)
+            outname = 'group_jaccard_median_%s_mesh140_%s.stratified_2.1D' % (h, td)
+            assigner(inname, outname)
