46b5ba2ed53ebada5deb6573be2b2afb8a5df0a6
Michael Andric <andric.michael@gmail.com>
2015-12-04 14:19:01 +0100

    init do_undump and update general_procedures

---
+++ do_undump.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  4 14:07:18 2015
+
+@author: andric
+"""
+
+import os
+import general_procedures as gp
+
+if __name__ == '__main__':
+    subj_list = []
+    for i in range(1, 20):
+        subj_list.append('hel%d' % i)
+    subj_list.remove('hel9')   # because this is bad subj
+
+    ava_dir = os.path.join(os.environ['hel'], 'ava')
+
+    for ss in subj_list:
+        vol_dir_pref = '%s/volume.%s.anat' % (ss, ss)
+        anat_dir = os.path.join(os.environ['hel'], vol_dir_pref)
+        
+        proc_dir = os.path.join(os.environ['hel'], ss, 'preprocessing')
+        ijk_name = os.path.join(anat_dir,
+                                '%s_gm_mask_frac_bin_ijk.txt' % ss)
+        master_file = os.path.join(anat_dir,
+                                   '%s_gm_mask_frac_bin.nii.gz' % ss)
+        for i in range(1, 3):
+            ava_name = 'ava_task_sess_%s_%s_gm_mskd.txt' % (i, ss)
+            ava_file = os.path.join(proc_dir, ava_name)
+            gp.undump(ss, ijk_name, ava_file, ava_dir, master_file, 'float')
--- general_procedures.py
+++ general_procedures.py
@@ -23,9 +23,9 @@ def undump(subjid, ijk_coords, datafilename, data_dir,
     GIVE FULL PATH
     Writes AFNI format Undumped file
     """
-    print 'Doing undump... First pasting ijk to data'
-    print 'Data type is %s' % dtype
-    print time.ctime()
+    print ('Doing undump... First pasting ijk to data')
+    print ('Data type is %s' % dtype)
+    print (time.ctime())
     ijkfile = np.loadtxt(ijk_coords)
     data = np.loadtxt(datafilename)
     data_ijk_outname = '%s.ijk' % datafilename
@@ -35,7 +35,7 @@ def undump(subjid, ijk_coords, datafilename, data_dir,
     stdout_dir = os.path.join(data_dir, 'stdout_files')
     if not os.path.exists(stdout_dir):
         os.makedirs(stdout_dir)
-    print 'Doing 3dUndump... %s' % time.ctime()
+    print ('Doing 3dUndump... %s' % time.ctime())
     f = open('%s/stdout_from_undump.txt' % stdout_dir, 'w')
     cmdargs = split('3dUndump -prefix %s -ijk \
                     -datum %s -master %s %s' %
@@ -43,8 +43,8 @@ def undump(subjid, ijk_coords, datafilename, data_dir,
                      master_file, data_ijk_outname))
     call(cmdargs, stdout=f, stderr=STDOUT)
     f.close()
-    print 'Finished undump '
-    print time.ctime()
+    print ('Finished undump ')
+    print (time.ctime())
 
 
 def avgepis(ss, epi_list, work_dir, outpref):
@@ -54,7 +54,7 @@ def avgepis(ss, epi_list, work_dir, outpref):
     :param epi_list: Include list of epis that you average over
     Writes to file a TS average brain
     """
-    print 'Doing avgepis for %s -- ' % ss+time.ctime()
+    print ('Doing avgepis for %s -- ' % ss+time.ctime())
     stdout_dir = os.path.join(work_dir, 'stdout_files')
     if not os.path.exists(stdout_dir):
         os.makedirs(stdout_dir)
@@ -73,8 +73,8 @@ def mean_epi(ss, infile, work_dir, outpref):
     :param ss: Subject identifier
     Writes to file AFNI mean brain (one image)
     """
-    print 'Doing mean_epi for %s -- ' % ss
-    print time.ctime()
+    print ('Doing mean_epi for %s -- ' % ss)
+    print (time.ctime())
     stdout_dir = os.path.join(work_dir, 'stdout_files')
     if not os.path.exists(stdout_dir):
         os.makedirs(stdout_dir)
@@ -88,7 +88,7 @@ def converttoNIFTI(work_dir, brain, prefix=None):
     """
     convert AFNI file to NIFTI
     """
-    print 'Doing converttoNIFTI for %s -- ' % brain+time.ctime()
+    print ('Doing converttoNIFTI for %s -- ' % brain+time.ctime())
     stdout_dir = os.path.join(work_dir, 'stdout_files')
     if not os.path.exists(stdout_dir):
         os.makedirs(stdout_dir)
@@ -105,7 +105,7 @@ def epi_reg(ss, work_dir, epi, wholet1, extrt1, out):
     """
     Register epi to t1
     """
-    print 'Doing epi_reg for %s -- ' % ss+time.ctime()
+    print ('Doing epi_reg for %s -- ' % ss+time.ctime())
     stdout_dir = os.path.join(work_dir, 'stdout_files')
     if not os.path.exists(stdout_dir):
         os.makedirs(stdout_dir)
@@ -120,7 +120,7 @@ def flirt_solo(work_dir, epi, extrt1, wm_edge, epi_reg_out, interp=None):
     """
     Doing flirt
     """
-    print 'doing flirt for %s -- ' % epi+time.ctime()
+    print ('doing flirt for %s -- ' % epi+time.ctime())
     stdout_dir = os.path.join(work_dir, 'stdout_files')
     if not os.path.exists(stdout_dir):
         os.makedirs(stdout_dir)
@@ -142,7 +142,7 @@ def applywarpFLIRT(ss, work_dir, input, extrt1, out, premat, interp=None):
     """
     Warp via linear transformation via fsl FLIRT
     """
-    print 'doing applywarpFLIRT for %s -- ' % ss+time.ctime()
+    print ('doing applywarpFLIRT for %s -- ' % ss+time.ctime())
     stdout_dir = os.path.join(work_dir, 'stdout_files')
     if not os.path.exists(stdout_dir):
         os.makedirs(stdout_dir)
@@ -161,7 +161,7 @@ def applywarpFNIRT(ss, work_dir, input, out, coeff, interp=None):
     """
     Warp via nonlinear transformation via fsl FNIRT
     """
-    print 'Doing applywarpFNIRT for %s -- ' % ss+time.ctime()
+    print ('Doing applywarpFNIRT for %s -- ' % ss+time.ctime())
     stdout_dir = os.path.join(work_dir, 'stdout_files')
     if not os.path.exists(stdout_dir):
         os.makedirs(stdout_dir)
@@ -201,16 +201,16 @@ def maskdump(stdout_dir, mask, in_pref, out_pref, noijk=True):
 
 
 def get_ijk(outname, mask=None):
-    print 'Getting IJK coordinates... '+time.ctime()
+    print ('Getting IJK coordinates... '+time.ctime())
     if mask is None:
-        print 'NOTT USING MASK. HUH?'
+        print ('NOTT USING MASK. HUH?')
         call("3dmaskdump %s | awk '{print $1, $2, $3}' > %s" %
              (mask, outname), shell=True)
     else:
-        print 'USING MASK: '+mask
+        print ('USING MASK: '+mask)
         call("3dmaskdump -mask %s %s | awk '{print $1, $2, $3}' > %s" %
              (mask, mask, outname), shell=True)
-    print 'DONE. '+time.ctime()
+    print ('DONE. '+time.ctime())
 
 
 def vol2surf_mni(work_dir, hemi, parent, pn, outname):
@@ -218,8 +218,8 @@ def vol2surf_mni(work_dir, hemi, parent, pn, outname):
     Project to MNI surf.
     Make sure 'suma_dir' is set right
     """
-    print 'Doing vol2surf_mni -- %s' % time.ctime()
-    print os.getcwd()
+    print ('Doing vol2surf_mni -- %s' % time.ctime())
+    print (os.getcwd())
     suma_dir = '/cnari/normal_language/apps/suma_MNI_N27'
     stdout_dir = '%s/stdout_files' % work_dir
     if not os.path.exists(stdout_dir):
@@ -247,8 +247,8 @@ def vol2surf_indiv(work_dir, spec, smoothwm, pial,
                    parent, surfvol, outname):
     """
     """
-    print 'Doing vol2surf_indiv -- %s' % time.ctime()
-    print os.getcwd()
+    print ('Doing vol2surf_indiv -- %s' % time.ctime())
+    print (os.getcwd())
     stdout_dir = '%s/stdout_files' % work_dir
     if not os.path.exists(stdout_dir):
         os.makedirs(stdout_dir)
@@ -268,8 +268,8 @@ def resamp_with_master(stdout_dir, inset, master, out_pref):
     """
     resampling with master dset
     """
-    print 'Doing resample -- %s' % time.ctime()
-    print os.getcwd()
+    print ('Doing resample -- %s' % time.ctime())
+    print (os.getcwd())
     if not os.path.exists(stdout_dir):
         os.makedirs(stdout_dir)
     f = open('%s/stdout_from_resamp.txt' % stdout_dir, 'w')
@@ -284,7 +284,7 @@ def fwhm_est(input_data, outname, mask=None):
     Estiamte FWHM of data
     Will return FWHM
     """
-    print 'Doing fwhm_est -- %s' % time.ctime()
+    print ('Doing fwhm_est -- %s' % time.ctime())
     stdout_dir = 'stdout_files'
     if not os.path.exists(stdout_dir):
         os.makedirs(stdout_dir)
@@ -303,7 +303,7 @@ def clustsim(fwhm, outdir, mask=None):
     """
     Find the size of clusters by chance
     """
-    print 'Running clustsim -- %s' % time.ctime()
+    print ('Running clustsim -- %s' % time.ctime())
     stdout_dir = 'stdout_files'
     if not os.path.exists(stdout_dir):
         os.makedirs(stdout_dir)
