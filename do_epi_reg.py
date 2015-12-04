e4dd817fa3a5e3a7859b32e291356a9647773a41
Michael Andric <andric.michael@gmail.com>
2015-12-04 14:33:39 +0100

    update do_epi_reg.py for degrees

--- do_epi_reg.py
+++ do_epi_reg.py
@@ -16,9 +16,7 @@ if __name__ == '__main__':
     subj_list.remove('hel9')   # because this is bad subj
 
     graph_dir = os.path.join(os.environ['hel'], 'graph_analyses')
-    conn_dir = os.path.join(os.environ['hel'], 'ccf_cor')
 
-    t_dict = {'vals': 'trilinear', 'lag': 'nn'}
     for ss in subj_list:
         vol_dir_pref = '%s/volume.%s.anat' % (ss, ss)
         anat_dir = os.path.join(os.environ['hel'], vol_dir_pref)
@@ -27,19 +25,23 @@ if __name__ == '__main__':
 
         premat = os.path.join(anat_dir,
                               '%s_gm_mask_frac_bin_flirted.mat' % ss)
-        for lb in t_dict:
-            epi_nii_pref = os.path.join(conn_dir,
-                                        'ccf_abs_%s_out_%s_gm_mskd.ijk' % (lb, ss))
-            gp.converttoNIFTI(conn_dir, '%s+orig' % epi_nii_pref,
-                              epi_nii_pref)
-            in_fl = '%s.nii.gz' % epi_nii_pref
-            out_fl = '%s_flirted' % epi_nii_pref
-            gp.applywarpFLIRT(ss, conn_dir, in_fl, extrt1,
-                              out_fl, premat, t_dict[lb])
-
-            fn_coef = os.path.join(anat_dir,
-                                   'T1_to_MNI_nonlin_coeff.nii.gz')
-            in_fn = '%s.nii.gz' % out_fl
-            out_fn = '%s_fnirted_MNI2mm' % epi_nii_pref
-            gp.applywarpFNIRT(ss, conn_dir, in_fn, out_fn,
-                              fn_coef, t_dict[lb])
+        
+        deg_dir = os.path.join(graph_dir, ss, 'degrees')
+        for i in range(1, 3):
+            for thr in [.05, .1, .15, .2]:
+                epi_nii_pref = os.path.join(deg_dir,
+                                            'task_sess_%s_%s.dens_%s.degrees.txt.ijk' %
+                                            (i, ss, thr))
+                gp.converttoNIFTI(deg_dir, '%s+orig' % epi_nii_pref,
+                                  epi_nii_pref)
+                in_fl = '%s.nii.gz' % epi_nii_pref
+                out_fl = '%s_flirted' % epi_nii_pref
+                gp.applywarpFLIRT(ss, deg_dir, in_fl, extrt1,
+                                  out_fl, premat, 'nn')
+    
+                fn_coef = os.path.join(anat_dir,
+                                       'T1_to_MNI_nonlin_coeff.nii.gz')
+                in_fn = '%s.nii.gz' % out_fl
+                out_fn = '%s_fnirted_MNI2mm' % epi_nii_pref
+                gp.applywarpFNIRT(ss, deg_dir, in_fn, out_fn,
+                                  fn_coef, 'nn')
