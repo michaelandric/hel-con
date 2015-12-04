73b2a62de4b810d4f2c5fb7283f20987e6082346
Michael Andric <andric.michael@gmail.com>
2015-12-04 15:45:57 +0100

    updated do_proc to resamp and maskdump degree data

--- do_proc.py
+++ do_proc.py
@@ -16,30 +16,31 @@ if __name__ == '__main__':
     subj_list.remove('hel9')   # because this is bad subj
 
     graph_dir = os.path.join(os.environ['hel'], 'graph_analyses')
-    t_dict = {'vals': 'float', 'lag': 'short'}
 
     mask_dir = '%s/group_anat' % (os.environ['hel'])
     mask_n = 'group_avg_gm_mask_frac_bin_fnirted_MNI4mm_thr0.5.nii.gz'
     mask_fname = os.path.join(mask_dir, mask_n)
 
-    ava_dir = os.path.join(os.environ['hel'], 'ava')
     out_res_suff = 'ijk_fnirted_MNI4mm_thr0.5'
     for ss in subj_list:
+        deg_dir = os.path.join(os.environ['hel'], 'graph_analyses',
+                               ss, 'degrees')
         vol_dir_pref = '%s/volume.%s.anat' % (ss, ss)
         anat_dir = os.path.join(os.environ['hel'], vol_dir_pref)
-        st_odir = os.path.join(ava_dir, 'stdout_dir')
+        st_odir = os.path.join(deg_dir, 'stdout_dir')
         if not os.path.exists(st_odir):
             os.makedirs(st_odir)
         for i in range(1, 3):
-            epi_nii_pref = os.path.join(ava_dir,
-                                        'ava_task_sess_%s_%s_gm_mskd.txt' %
-                                        (i, ss))
-            in_resamp_pref = os.path.join(ava_dir,
-                                          '%s.ijk_fnirted_MNI2mm' % epi_nii_pref)
-            out_resamp_pref = os.path.join(ava_dir,
-                                           '%s.%s' % (epi_nii_pref, out_res_suff))
-            gp.resamp_with_master(ava_dir, '%s.nii.gz' % in_resamp_pref,
-                                  mask_fname, '%s.nii.gz' % out_resamp_pref)
-            out_fname = os.path.join(ava_dir, '%s.txt' % out_resamp_pref)
-            gp.maskdump(st_odir, mask_fname,
-                        '%s.nii.gz' % out_resamp_pref, out_fname)
+            for thr in [.05, .1, .15, .2]:
+                epi_nii_pref = os.path.join(deg_dir,
+                                            'task_sess_%s_%s.dens_%s.degrees.txt' %
+                                            (i, ss, thr))
+                in_resamp_pref = os.path.join(deg_dir,
+                                              '%s.ijk_fnirted_MNI2mm' % epi_nii_pref)
+                out_resamp_pref = os.path.join(deg_dir,
+                                               '%s.%s' % (epi_nii_pref, out_res_suff))
+                gp.resamp_with_master(deg_dir, '%s.nii.gz' % in_resamp_pref,
+                                      mask_fname, '%s.nii.gz' % out_resamp_pref)
+                out_fname = os.path.join(deg_dir, '%s.txt' % out_resamp_pref)
+                gp.maskdump(st_odir, mask_fname,
+                            '%s.nii.gz' % out_resamp_pref, out_fname)
