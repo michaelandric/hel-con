b2d5183bc16c64ff2af96ebe046e0523f7ad861a
Michael Andric <andric.michael@gmail.com>
2015-12-06 15:39:32 +0100

    do_undump for ava_smth

--- do_undump.py
+++ do_undump.py
@@ -14,17 +14,18 @@ if __name__ == '__main__':
         subj_list.append('hel%d' % i)
     subj_list.remove('hel9')   # because this is bad subj
 
+    ava_dir = os.path.join(os.environ['hel'], 'ava')
+
     for ss in subj_list:
         vol_dir_pref = '%s/volume.%s.anat' % (ss, ss)
         anat_dir = os.path.join(os.environ['hel'], vol_dir_pref)
         
-        deg_dir = os.path.join(os.environ['hel'], 'graph_analyses', ss, 'degrees')
+        proc_dir = os.path.join(os.environ['hel'], ss, 'preprocessing')
         ijk_name = os.path.join(anat_dir,
                                 '%s_gm_mask_frac_bin_ijk.txt' % ss)
         master_file = os.path.join(anat_dir,
                                    '%s_gm_mask_frac_bin.nii.gz' % ss)
         for i in range(1, 3):
-            for thr in [.05, .1, .15, .2]:
-                deg_name = 'task_sess_%s_%s.dens_%s.degrees.txt' % (i, ss, thr)
-                deg_file = os.path.join(deg_dir, deg_name)
-                gp.undump(ss, ijk_name, deg_file, deg_dir, master_file)
+            ava_name = 'ava_smth_task_sess_%s_%s_gm_mskd.txt' % (i, ss)
+            ava_file = os.path.join(proc_dir, ava_name)
+            gp.undump(ss, ijk_name, ava_file, ava_dir, master_file, 'float')
