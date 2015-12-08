c6749ccf8870946d31f6e42ba0851b998b46c550
Michael Andric <andric.michael@gmail.com>
2015-12-08 15:40:32 +0100

    do_undump for ava_deg _correlation data

--- do_undump.py
+++ do_undump.py
@@ -15,17 +15,15 @@ if __name__ == '__main__':
     subj_list.remove('hel9')   # because this is bad subj
 
     ava_dir = os.path.join(os.environ['hel'], 'ava')
+    anat_dir = os.path.join(os.environ['hel'], 'group_anat')
 
-    for ss in subj_list:
-        vol_dir_pref = '%s/volume.%s.anat' % (ss, ss)
-        anat_dir = os.path.join(os.environ['hel'], vol_dir_pref)
-        
-        proc_dir = os.path.join(os.environ['hel'], ss, 'preprocessing')
-        ijk_name = os.path.join(anat_dir,
-                                '%s_gm_mask_frac_bin_ijk.txt' % ss)
-        master_file = os.path.join(anat_dir,
-                                   '%s_gm_mask_frac_bin.nii.gz' % ss)
-        for i in range(1, 3):
-            ava_name = 'ava_smth_task_sess_%s_%s_gm_mskd.txt' % (i, ss)
-            ava_file = os.path.join(proc_dir, ava_name)
-            gp.undump(ss, ijk_name, ava_file, ava_dir, master_file, 'float')
+    ijk_name = os.path.join(anat_dir,
+                            'group_avg_gm_mask_frac_bin_fnirted_MNI4mm_thr0.5_ijk.txt')
+    master_file = os.path.join(anat_dir,
+                               'group_avg_gm_mask_frac_bin_fnirted_MNI4mm_thr0.5.nii.gz')
+
+    for thr in [.05, .1, .15, .2]:
+        for r in range(1, 3):
+            ava_name = 'ava_smth_deg_corr_sess_%s_dens_%s_out' % (r, thr)
+            ava_file = os.path.join(ava_dir, ava_name)
+            gp.undump('group', ijk_name, ava_file, ava_dir, master_file, 'float')
