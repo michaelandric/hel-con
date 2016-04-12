db56b0c7f64352c7a8c3a3747a3064b7fc5a7fbf
Michael Andric <andric.michael@gmail.com>
2016-04-12 16:35:02 +0200

    mod do_avg_global_conn to get average of first and second times
    Get mean of runs 1 and 3, then runs 4 and 6

--- do_avg_global_conn.py
+++ do_avg_global_conn.py
@@ -23,12 +23,26 @@ if __name__ == '__main__':
         proc_dir = os.path.join(os.environ['hel'], ss, 'preprocessing')
         conn_dir = os.path.join(top_dir,
                                 '{}/single_run_global_connectivity'.format(ss))
+        """
         if not os.path.exists(conn_dir):
             os.makedirs(conn_dir)
         for r in [1, 3, 4, 6]:
             ts_name = os.path.join(proc_dir,
-                                   'task_r0{}_{}_gm_mskd'.format(r, ss))
+                                   'task_r0{}_{}_gm_mskd.txt'.format(r, ss))
             agc = ge.avg_global_connectivity(ts_name)
             out_name = 'avg_corrZ_task_r0{}_{}'.format(r, ss)
             out_fname = os.path.join(conn_dir, out_name)
             np.savetxt(out_fname, agc, fmt='%.4f')
+        """
+        corrZ_files = []
+        for r in [1, 3, 4, 6]:
+            corrZ_files.append(np.loadtxt(
+            os.path.join(conn_dir, 'avg_corrZ_task_r0{}_{}'.format(r, ss))))
+
+        first = np.array([corrZ_files[0], corrZ_files[1]]).mean(0)
+        first_name = 'avg_corrZ_task_runs1and3_{}'.format(ss)
+        np.savetxt(first_name, first, fmt='%.4f')
+
+        second = np.array([corrZ_files[2], corrZ_files[3]]).mean(0)
+        second_name = 'avg_corrZ_task_runs4and6_{}'.format(ss)
+        np.savetxt(second_name, second, fmt='%.4f')
