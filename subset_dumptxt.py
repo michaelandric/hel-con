a6edd37f8c775156d3fb3d26806ef8a34b95e1f0
Michael Andric <andric.michael@gmail.com>
2016-04-12 14:49:42 +0200

    mod subset_dumptxt to get runs 1 3 4 and 6

--- subset_dumptxt.py
+++ subset_dumptxt.py
@@ -5,6 +5,9 @@ Created on Fri May 29 14:05:42 2015
 Grab the TS of interest via 3dTcat
 Then dump that to text file for further analysis.
 
+Update: Tues Apr 12 2016
+Addressing whether being in scanner longer gives effect
+Compare runs 1 & 4 to 3 & 6
 @author: andric
 """
 
@@ -35,16 +38,16 @@ def maskdump(stdout_dir, mask, in_pref, out_pref):
 
 
 def get_ijk(outname, mask=None):
-    print 'Getting IJK coordinates... '+time.ctime()
+    print ('Getting IJK coordinates... '.format+time.ctime())
     if mask is None:
-        print 'NOTT USING MASK. HUH?'
-        call("3dmaskdump %s | awk '{print $1, $2, $3}' > %s" %
-             (mask, outname), shell=True)
+        print ('NOTT USING MASK. HUH?')
+        call("3dmaskdump {} | awk '{print $1, $2, $3}' > {}".format(
+             (mask, outname)), shell=True)
     else:
-        print 'USING MASK: '+mask
-        call("3dmaskdump -mask %s %s | awk '{print $1, $2, $3}' > %s" %
-             (mask, mask, outname), shell=True)
-    print 'DONE. '+time.ctime()
+        print ('USING MASK: {}'.format(mask))
+        call("3dmaskdump -mask {} {} | awk '{print $1, $2, $3}' > {}".format(
+             (mask, mask, outname)), shell=True)
+    print ('DONE. {}'.format(time.ctime()))
 
 
 def build_inputlist(subjid, proc_dir, sess):
@@ -76,34 +79,49 @@ def build_inputlist(subjid, proc_dir, sess):
     return inp_list
 
 
+def build_subset_runs(subjid, proc_dir, runs):
+    """
+    this cuts to runs 1 and 3 (and 4 and 6)
+    """
+    inp_list = []
+#    runs = [1, 3, 4, 6]
+    subs = ['9..78, 108..210', '9..40, 63..131, 155..220']
+    sub_dct = dict(zip(runs[:2], subs))
+    sub_dct.update(dict(zip(runs[2:], subs)))
+    for r in sub_dct:
+        f = "'{}/cleanTS_{}r0{}_smth4mm_Liresamp4mm_gm_mskd+orig[{}]'".format(
+        proc_dir, subjid, int(r), sub_dct[r])
+        inp_list.append(f)
+    
+    return inp_list
+    
+
 if __name__ == '__main__':
     subj_list = ['hel19']
     for ss in subj_list:
-        print 'Doing subject %s' % ss
-        print time.ctime()
+        print ('Doing subject {}'.format(ss))
+        print (time.ctime())
         preproc_dir = os.path.join(os.environ['hel'],
                                    '%s/preprocessing/' % ss)
         if not os.path.exists(preproc_dir):
-            print 'Problem with paths.'
-            print 'Shut down!'
+            print ('Problem with paths.')
+            print ('Shut down!')
             sys.exit(1)
 
         stdout_dir = os.path.join(preproc_dir, 'stdout_files/')
         if not os.path.exists(stdout_dir):
             os.makedirs(stdout_dir)
-
-        for session in range(1, 3):
-            in_list = build_inputlist(ss, preproc_dir, session)
-            cat_out_name = '%s/task_sess_%d_%s_gm_mskd' % (preproc_dir,
-                                                           session, ss)
+        
+        runs = [1, 3, 4, 6]
+        for r in runs:
+            in_list = build_subset_runs(ss, preproc_dir, runs)
+            cat_out_name = os.path.join(preproc_dir,
+                                        'task_sess_r0{}_{}_gm_mskd'.format(r, ss))
             TScat(stdout_dir, in_list, cat_out_name)
 
             mask_dir = '%s/%s/volume.%s.anat/' % (os.environ['hel'], ss, ss)
             mask_pref = os.path.join(mask_dir, '%s_gm_mask_frac_bin' % ss)
             mask = '%s.nii.gz' % mask_pref
-            in_name = '%s+orig' % cat_out_name
-            ts_outname = '%s.txt' % cat_out_name
+            in_name = '{}+orig'.format(cat_out_name)
+            ts_outname = '{}.txt'.format(cat_out_name)
             maskdump(stdout_dir, mask, in_name, ts_outname)
-
-        ijk_outname = '%s_ijk.txt' % mask_pref
-        get_ijk(ijk_outname, mask)
