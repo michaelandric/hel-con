8c2a7fd4385aea80e0807bd3dabcaff43b4a9d81
Michael Andric <andric.michael@gmail.com>
2016-04-13 22:48:24 +0200

    update do_t_test to do runs1and3 vs. runs4and6

--- do_t_test.py
+++ do_t_test.py
@@ -6,10 +6,8 @@ Created on Sat Jun 20 17:12:16 2015
 """
 
 import os
-import sys
 from shlex import split
 from subprocess import call, STDOUT
-import logging
 
 
 def t_test(stdoutdir, ss_list, outpref):
@@ -33,6 +31,36 @@ def t_test(stdoutdir, ss_list, outpref):
                     -paired -prefix %s' %
                     (a_sets, b_sets,
                      '%s/data/standard' % os.environ['FSLDIR'], outpref))
+    call(cmdargs, stdout=f, stderr=STDOUT)
+    f.close()
+
+
+def t_test_subruns(stdoutdir, ss_list, outpref):
+    """
+    testing 1st and 3rd runs versus 4th and 6th
+    """
+    top_dir = os.path.join(os.environ['hel'], 'graph_analyses')
+    a_sets = []
+    b_sets = []
+    for ss in ss_list:
+        pref_a = 'avg_corrZ_task_runs1and3_{}.ijk'.format(ss)
+        pref_b = 'avg_corrZ_task_runs4and6_{}.ijk'.format(ss)
+
+        a_sets.append(os.path.join(top_dir, ss,
+                                   'single_run_global_connectivity', pref_a))
+        b_sets.append(os.path.join(top_dir, ss,
+                                   'single_run_global_connectivity', pref_b))
+    a_sets = ' '.join(a_sets)
+    b_sets = ' '.join(b_sets)
+
+    f = open(os.path.join(stdoutdir, 'stdout_from_3dttest++.txt'), 'w')
+    cmdargs = split('3dttest++ -setA {} -labelA sess_1 -setB {} -labelB sess_2 \
+                    -mask {} -paired -prefix {}'.format(
+                    a_sets, b_sets,
+                    os.path.join(os.environ['FSLDIR'],
+                                 'data/standard', 
+                                 'MNI152_T1_2mm_brain_mask_dil1.nii.gz'),
+                    outpref))
     call(cmdargs, stdout=f, stderr=STDOUT)
     f.close()
 
@@ -135,15 +163,14 @@ def ava_main(stdoutdir, ss_list, outpref):
     
 
 if __name__ == '__main__':
-    subj_list = []
-    for i in range(1, 20):
-        subj_list.append('hel%d' % i)
+    subj_list = ['hel{}'.format(i) for i in range(1, 20)]
     subj_list.remove('hel9')   # because this is bad subj
 
-    out_dir = os.path.join(os.environ['hel'], 'ava')
+    out_dir = os.path.join(os.environ['hel'], 'graph_analyses',
+                           'group_single_run_global_connectivity')
     stdout_dir = os.path.join(out_dir, 'stdout_files')
     if not os.path.exists(stdout_dir):
         os.makedirs(stdout_dir)
 
-    outpref = os.path.join(out_dir, 'ttest_out_ava_smth')
-    ava_main(stdout_dir, subj_list, outpref)
+    outpref = os.path.join(out_dir, 'ttest_subruns')
+    t_test_subruns(stdout_dir, subj_list, outpref)
