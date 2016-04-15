c0baeca73bc36b2c8961ce9f2b4855e87526b596
Michael Andric <andric.michael@gmail.com>
2016-04-15 14:24:55 +0200

    getting difference between 1 and 3, 4 and 6

--- do_calc_diff.py
+++ do_calc_diff.py
@@ -2,6 +2,9 @@
 """
 Created on Wed Dec  9 16:10:25 2015
 
+Update Apr 15 2016 - Using to get
+diff between runs
+
 @author: andric
 """
 
@@ -15,22 +18,29 @@ def calc(input1, input2, outpref):
     3dcalc -a Corr_subj01R+tlrc -expr 'atanh(a)' -prefix Corr_subj01_Z
     """
     logging.info('Doing 3dcalc for %s' % outpref)
-    cmd = split("3dcalc -a %s -b %s -expr 'a-b' -prefix %s" %
-                (input1, input2, outpref))
+    cmd = split("3dcalc -a {} -b {} -expr 'a-b' -prefix {}".format(input1,
+                input2, outpref))
     call(cmd)
 
 
-ava_dir = os.path.join(os.environ['hel'], 'ava')
-logging.basicConfig(filename='%s/calc_diff.log' % ava_dir, level=logging.INFO)
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
+if __name__ == '__main__':
+    subj_list = ['hel{}'.format(i) for i in range(1, 20) if i is not 9]
 
-for thr in [.05, .1, .15, .2]:
-    ava1 = ('%s/ava_smth_deg_corr_sess_1_dens_%s_out.ijk_Z+tlrc' %
-            (ava_dir, thr))
-    ava2 = ('%s/ava_smth_deg_corr_sess_2_dens_%s_out.ijk_Z+tlrc' %
-            (ava_dir, thr))
-    outpref = ('%s/ava_smth_deg_corr_diff_dens_%s_out.ijk_Z' % (ava_dir, thr))
-    calc(ava1, ava2, outpref)
+    sfx = 'ijk_fnirted_MNI2mm.nii.gz'
+    for ss in subj_list:
+        conn_dir = os.path.join(os.environ['hel'], 'graph_analyses',
+                                '{}/single_run_global_connectivity'.format(ss))
+        logging.basicConfig(filename=os.path.join(conn_dir, 'calc_diff.log'),
+                            level=logging.INFO)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
 
-logging.info('3dcalc done.')
+        f1 = os.path.join(conn_dir, 'avg_corrZ_task_r01_{}.{}'.format(ss, sfx))
+        f3 = os.path.join(conn_dir, 'avg_corrZ_task_r03_{}.{}'.format(ss, sfx))
+        calc(f1, f3, os.path.join(conn_dir, 'diff_avg_corrZ_task_runs1and3'))
+
+        f4 = os.path.join(conn_dir, 'avg_corrZ_task_r04_{}.{}'.format(ss, sfx))
+        f6 = os.path.join(conn_dir, 'avg_corrZ_task_r06_{}.{}'.format(ss, sfx))        
+        calc(f4, f6, os.path.join(conn_dir, 'diff_avg_corrZ_task_runs4and6'))
+
+    logging.info('3dcalc done')
