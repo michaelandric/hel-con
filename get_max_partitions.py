3185a157c35990e1bc52e987f5abe2aa375cd786
Michael Andric <andric.michael@gmail.com>
2016-04-20 15:12:58 +0200

    edit get_max_partitions for indiv sub runs

--- get_max_partitions.py
+++ get_max_partitions.py
@@ -21,24 +21,18 @@ if __name__ == '__main__':
         proc_dir = os.path.join(os.environ['hel'], ss, 'preprocessing')
         mod_dir = os.path.join(top_dir, ss, 'subrun_modularity')
 
-        for r in [1, 3]:
-            if r is 1:
-                session = 'first'
-            if r is 3:
-                session = 'second'
+        for r in [1, 3, 4, 6]:
             ts_name = os.path.join(proc_dir,
                                    'task_r0{}_{}_gm_mskd.txt'.format(r, ss))
             graph_dir = os.path.join(top_dir, ss, 'subrun_graphs')
             for thresh_dens in [.15]:
+                pref = 'task_r0{}_{}.dens_{}'.format(r, ss, thresh_dens)
                 gr = ge.Graphs(ss, ts_name, thresh_dens, graph_dir)
-                q_fname = 'task_{}_{}.dens_{}.Qval'.format(
-                    session, ss, thresh_dens)
+                q_fname = '{}.Qval'.format(pref)
                 qval, iter_max = gr.max_q(os.path.join(mod_dir, q_fname))
-                trees_fname = 'task_{}_{}.dens_{}.trees'.format(
-                    session, ss, thresh_dens)
+                trees_fname = '{}.trees'.format(pref)
                 trees = np.loadtxt(os.path.join(mod_dir, trees_fname))
                 best_tree = trees[:, iter_max]+1
-                best_tree_fname = 'task_{}_{}.dens_{}.maxq_tree'.format(
-                    session, ss, thresh_dens)
+                best_tree_fname = '{}.maxq_tree'.format(pref)
                 np.savetxt(os.path.join(mod_dir, best_tree_fname),
                            best_tree, fmt='%i')
