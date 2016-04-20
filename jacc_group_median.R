b2190dd40bec5b7ff8c053918a643891130efd33
Michael Andric <andric.michael@gmail.com>
2016-04-20 16:24:14 +0200

    update jacc_group_median to get indiv 1and4 and 3and6

--- jacc_group_median.R
+++ jacc_group_median.R
@@ -11,21 +11,24 @@ grp_jacc_dir <- paste(graph_dir, 'subrun_group_jaccard/', sep='')
 nnodes <- 196002
 
 #for (td in seq(.05, .2, .05))
-for (td in c(.15))
+for (nom in c('1and4', '3and6'))
 {
-    for (h in c('lh', 'rh'))
+    for (td in c(.15))
     {
-        group_mat <- matrix(ncol=length(subjects), nrow=nnodes)
-        for (i in 1:length(subjects))
+        for (h in c('lh', 'rh'))
         {
-            ss <- subjects[i]
-            jacc_dir <- paste(graph_dir, ss, '/subrun_jaccard_res/', sep='')
-            fname <- paste('jacc_',ss,'_',td,'.ijk_',h,'_mesh140.1D', sep='')
-            group_mat[, i] <- read.table(paste(jacc_dir, fname, sep=''))$V2
+            group_mat <- matrix(ncol=length(subjects), nrow=nnodes)
+            for (i in 1:length(subjects))
+            {
+                ss <- subjects[i]
+                jacc_dir <- paste(graph_dir, ss, '/subrun_jaccard_res/', sep='')
+                fname <- paste('jacc_',ss,'_',td,'_',nom,'.ijk_',h,'_mesh140.1D', sep='')
+                group_mat[, i] <- read.table(paste(jacc_dir, fname, sep=''))$V2
+            }
+            grp_med <- apply(group_mat, 1, median)
+            grp_med_out <- cbind(seq(0, nnodes-1), grp_med)
+            outname <- paste(grp_jacc_dir, 'subrun_group_jaccard_median_', h,'_mesh140_', td, '_', nom,'.1D', sep='')
+            write.table(grp_med_out, outname, row.names=FALSE, col.names=FALSE, quote=FALSE)
         }
-        grp_med <- apply(group_mat, 1, median)
-        grp_med_out <- cbind(seq(0, nnodes-1), grp_med)
-        outname <- paste(grp_jacc_dir, 'subrun_group_jaccard_median_', h,'_mesh140_', td, '.1D', sep='')
-        write.table(grp_med_out, outname, row.names=FALSE, col.names=FALSE, quote=FALSE)
     }
 }
