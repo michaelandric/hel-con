dd952a5de91401bb6991711569674b894a4c43a4
Michael Andric <andric.michael@gmail.com>
2016-04-21 17:26:18 +0200

    update do_make_grp_mat.py for subrun modularity

--- do_make_grp_mat.py
+++ do_make_grp_mat.py
@@ -2,36 +2,33 @@
 """
 Created on Fri Jun 19 12:57:27 2015
 
+revision April 2016 for subrun modularity
+
 @author: andric
 """
 
 import os
 import numpy as np
 
-top_dir = '%s/graph_analyses' % os.environ['hel']
-nvox = 262245
+top_dir = (os.environ['hel'], 'graph_analyses')
+nvox = 20071
 suff = 'maxq_tree.ijk_fnirted_MNI4mm_thr0.5.txt'
 
 if __name__ == '__main__':
-    subj_list = []
-    for i in range(1, 20):
-        subj_list.append('hel%d' % i)
-    subj_list.remove('hel9')   # because this is bad subj
+    subj_list = ['hel{}'.format(i) for i in range(1, 20) if i is not 9]
 
-    top_dir = '%s/graph_analyses' % os.environ['hel']
-
-    suff = 'ijk_fnirted_MNI2mm.txt'
-    for clst in [2, 3]:
-        # already did clst1
+    thresh_dens = .15
+    for session in ['first', 'second']:
         grp_mat = np.zeros(nvox*len(subj_list))
         grp_mat = grp_mat.reshape(nvox, len(subj_list))
         for i, ss in enumerate(subj_list):
-            conn_dir = os.path.join(top_dir, '%s/global_connectivity' % ss)
-            f_name = '%s_Clust_msk_glob_conn_knnward_clst%d.%s' % \
-                     (ss, clst, suff)
-            in_fname = os.path.join(conn_dir, f_name)
+            mod_dir = os.path.join(top_dir, ss, 'subrun_modularity')
+            f_name = 'task_{}_{}.dens_{}.{}'.format(
+                session, ss, thresh_dens, suff)
+            in_fname = os.path.join(mod_dir, f_name)
             grp_mat[:, i] = np.loadtxt(in_fname)
-        out_f_name = 'group_Clust_msk_glob_conn_knnward_clst%d' % clst
+        out_f_name = 'group_task_sess_{}.dens_{}.{}'.format(
+            session, thresh_dens, suff)
         out_f = os.path.join(top_dir,
-                             'group_global_connectivity', out_f_name)
+                             'subrun_group_modularity_thr0.5msk', out_f_name)
         np.savetxt(out_f, grp_mat, fmt='%i')
