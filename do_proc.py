46da461d788ebf0d416fd7e8d0f90ab7ee6f9ac7
Michael Andric <andric.michael@gmail.com>
2016-04-21 15:06:56 +0200

    do undump for subruns

--- do_proc.py
+++ do_proc.py
@@ -10,25 +10,28 @@ import general_procedures as gp
 
 
 if __name__ == '__main__':
+    subj_list = ['hel{}'.format(i) for i in range(1, 20) if i is not 9]
 
-    subj_list = ['hel{}'.format(i) for i in range(1, 20)]
-    subj_list.remove('hel9')   # because this is bad subj
-
+    thresh_dens = .15
     top_dir = os.path.join(os.environ['hel'], 'graph_analyses')
+    mask_dir = os.path.join(os.environ['hel'], 'group_anat')
+    mask_n = 'group_avg_gm_mask_frac_bin_fnirted_MNI4mm_thr0.5.nii.gz'
+    mask_fname = os.path.join(mask_dir, mask_n)
     for ss in subj_list:
-        vol_dir_pref = '{}/volume.{}.anat'.format(ss, ss)
-        anat_dir = os.path.join(os.environ['hel'], vol_dir_pref)
-        conn_dir = os.path.join(top_dir,
-                                '{}/single_run_global_connectivity'.format(ss))
-        st_odir = os.path.join(conn_dir, 'stdout_dir')
+        anat_dir = os.path.join(os.environ['hel'], ss,
+                                'volume.{}.anat'.format(ss))
+        mod_dir = os.path.join(top_dir, ss, 'subrun_modularity')
+        st_odir = os.path.join(mod_dir, 'stdout_dir')
         if not os.path.exists(st_odir):
             os.makedirs(st_odir)
+
         ijk_name = os.path.join(anat_dir,
                                 '{}_gm_mask_frac_bin_ijk.txt'.format(ss))
         master_file = os.path.join(anat_dir,
                                    '{}_gm_mask_frac_bin.nii.gz'.format(ss))
 
-        for r in [1, 3, 4, 6]:
-            fname = os.path.join(conn_dir, 'avg_corrZ_task_r0{}_{}'.format(r, ss))
-            gp.undump(ss, ijk_name, fname, conn_dir, master_file, 'float')
-
+        for session in ['first', 'second']:
+            tree_name = os.path.join(mod_dir,
+                                     'task_{}_{}.dens_{}.maxq_tree'.format(
+                                     session, ss, thresh_dens))
+            gp.undump(ss, ijk_name, tree_name, mod_dir, master_file, 'short')
