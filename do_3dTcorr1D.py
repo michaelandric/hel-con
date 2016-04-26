38a26bf8c097326c2119a55e8360ba9553b3d7da
Michael Andric <andric.michael@gmail.com>
2016-04-26 17:20:43 +0200

    init do_3dTcorr1D.py
    correlating behavior scores against first view of global brain
    connectivity

---
+++ do_3dTcorr1D.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 26 16:34:22 2016
+
+Setting up 3dTcorr1D
+@author: andric
+"""
+
+import os
+import logging
+import subprocess
+from shlex import split
+
+
+def bucket(workdir, subj_list, outname):
+    logging.INFO('work directory: {}'.format(workdir))
+    graph_dir = os.path.join(os.environ['hel'], 'graph_analyses')
+
+    input_set = []
+    for ss in subj_list:
+        conn_dir = os.path.join(graph_dir, ss, 'global_connectivity')
+        fname = 'avg_corrZ_task_sess_1_{}.ijk_fnirted_MNI2mm.nii.gz'.format(ss)
+        input_set.append(os.path.join(conn_dir, fname))
+    input_set = ' '.join(input_set)
+
+    cmd = split('3dbucket -prefix {} {}'.format(outname, input_set))
+    logging.INFO('cmd: \n{}'.format(cmd))
+    subprocess.call(cmd, stdout=subprocess.PIPE)
+
+
+def tcorr1D(inbucket, outname):
+    logging.INFO('doing 3dTcorr1D')
+    scores = os.path.join(os.environ['hel'], 'questions', 'scores.txt')
+    cmd = split('3dTcorr1D -prefix {} {} {}'.format(outname, inbucket, scores))
+    logging.INFO('cmd: \n{}'.format(cmd))
+    subprocess.call(cmd, stdout=subprocess.PIPE)
+
+
+
+if __name__ == '__main__':
+    subj_list = ['hel{}'.format(i) for i in range(1, 20) if i is not 9]
+
+    workdir = os.path.join(os.environ['hel'], 'graph_analyses/behav_correlate')
+    logging.basicConfig(filename=os.path.join(workdir, 'bucket.log'),
+                        level=logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    outbucket = os.path.join(workdir, 'avg_corrZ_task_sess_1_bucket')
+    bucket(workdir, subj_list, outbucket)
+
+    logging.basicConfig(filename=os.path.join(workdir, '3dTcorr1D.log'),
+                        level=logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    outcorr = os.path.join(workdir, 'avg_corrZ_task_sess_1_corr_with_behav')
+    tcorr1D('{}+tlrc.'.format(outbucket), outcorr)
