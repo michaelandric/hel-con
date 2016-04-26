6aba6fb093d93b73419f0d9730f3d8ade0e2c196
Michael Andric <andric.michael@gmail.com>
2016-04-26 18:21:07 +0200

    edit do_3dTcorr1D.py for tcorr_group data
    correlate tcorr_group data against behavior scores

--- do_3dTcorr1D.py
+++ do_3dTcorr1D.py
@@ -13,14 +13,20 @@ from shlex import split
 
 
 def bucket(workdir, subj_list, outname):
+    logging.basicConfig(filename=os.path.join(workdir, 'bucket.log'),
+                        level=logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(message)s')
+    ch.setFormatter(formatter)
+
     logging.info('work directory: {}'.format(workdir))
-    graph_dir = os.path.join(os.environ['hel'], 'graph_analyses')
+    tcorr_dir = os.path.join(os.environ['hel'], 'tcorr_group')
 
     input_set = []
     for ss in subj_list:
-        conn_dir = os.path.join(graph_dir, ss, 'global_connectivity')
-        fname = 'avg_corrZ_task_sess_1_{}.ijk_fnirted_MNI2mm.nii.gz'.format(ss)
-        input_set.append(os.path.join(conn_dir, fname))
+        fname = 'tcorr_prsn_{}_gm_mskd_Z_fnirted_MNI2mm.nii.gz'.format(ss)
+        input_set.append(os.path.join(tcorr_dir, fname))
     input_set = ' '.join(input_set)
 
     cmd = split('3dbucket -prefix {} {}'.format(outname, input_set))
@@ -29,10 +35,16 @@ def bucket(workdir, subj_list, outname):
 
 
 def tcorr1D(inbucket, outname):
+    logging.basicConfig(filename=os.path.join(workdir, '3dTcorr1D.log'),
+                    level=logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(message)s')
+    ch.setFormatter(formatter)
+
     logging.info('doing 3dTcorr1D')
     scores = os.path.join(os.environ['hel'], 'questions', 'usable_scores.txt')
-    cmd = split('3dTcorr1D -prefix %s %s %s' % 
-                (outname, inbucket, scores))
+    cmd = split('3dTcorr1D -prefix {} {} {}'.format(outname, inbucket, scores))
     logging.info('cmd: \n{}'.format(cmd))
     subprocess.call(cmd, stdout=subprocess.PIPE)
 
@@ -40,18 +52,10 @@ def tcorr1D(inbucket, outname):
 
 if __name__ == '__main__':
     subj_list = ['hel{}'.format(i) for i in range(1, 20) if i is not 9]
-
     workdir = os.path.join(os.environ['hel'], 'graph_analyses/behav_correlate')
-    logging.basicConfig(filename=os.path.join(workdir, 'bucket.log'),
-                        level=logging.INFO)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    outbucket = os.path.join(workdir, 'avg_corrZ_task_sess_1_bucket')
+
+    outbucket = os.path.join(workdir, 'tcorr_prsn_gm_mskd_Z_bucket')
     bucket(workdir, subj_list, outbucket)
 
-    logging.basicConfig(filename=os.path.join(workdir, '3dTcorr1D.log'),
-                        level=logging.INFO)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    outcorr = os.path.join(workdir, 'avg_corrZ_task_sess_1_corr_with_behav')
+    outcorr = os.path.join(workdir, 'tcorr_prsn_gm_mskd_Z_corr_with_behav')
     tcorr1D('{}+tlrc.'.format(outbucket), outcorr)
