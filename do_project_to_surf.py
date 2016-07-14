471d6c794f800c722eadc2a800559137f33b66f1
michaelandric <andric.michael@gmail.com>
2016-07-14 17:21:33 +0200

    do_project_to_surf.py for the n10000 vals

--- do_project_to_surf.py
+++ do_project_to_surf.py
@@ -9,17 +9,16 @@ import os
 import general_procedures as gp
 
 
-WORKDIR = os.path.join(os.environ['hel'], 'graph_analyses/behav_correlate')
-SEED_PREFS = ['lh_highlevel', 'lh_ttg', 'lh_vis_ctx']
+WORKDIR = os.path.join(os.environ['hel'], 'graph_analyses',
+                       'randomise_global_connectivity')
 
-for seed in SEED_PREFS:
-    fname = 'wgc_sess_2_{}_corr_tvals_2.898_234_vals'.format(seed)
-    parent_pref = os.path.join(WORKDIR, fname)
-    if os.path.exists('{}+tlrc.HEAD'.format(parent_pref)):
-        for hemi in ['lh', 'rh']:
-            outname = '{}_{}_pn1.0_MNI_N27.1D'.format(parent_pref, hemi)
-            gp.vol2surf_mni(WORKDIR, hemi, '{}+tlrc'.format(parent_pref),
-                            '1.0', outname)
-    else:
-        print('There is no {}+tlrc.HEAD'.format(parent_pref))
-        print('On to the next one.')
+fname = 'wgc_PairedTres_n10000_clustere_corrp_tstat2_vals'
+parent_pref = os.path.join(WORKDIR, fname)
+if os.path.exists('{}+tlrc.HEAD'.format(parent_pref)):
+    for hemi in ['lh', 'rh']:
+        outname = '{}_{}_pn1.0_MNI_N27.1D'.format(parent_pref, hemi)
+        gp.vol2surf_mni(WORKDIR, hemi, '{}+tlrc'.format(parent_pref),
+                        '1.0', outname)
+else:
+    print('There is no {}+tlrc.HEAD'.format(parent_pref))
+    print('On to the next one.')
